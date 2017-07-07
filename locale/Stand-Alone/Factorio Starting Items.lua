558a00e4275bd3a30b332f7a50568a50a195f45a
Cooldude2606 <cooldude260607@gmail.com>
2017-07-07 23:01:08 +0100

    Added Stating Items In stand Alone

--- locale/Stand-Alone/Factorio Starting Items.lua
+++ locale/Stand-Alone/Factorio Starting Items.lua
@@ -9,15 +9,28 @@ Discord: https://discord.gg/XSsBV6b
 The credit below may be used by another script do not remove.
 ]]
 local credits = {{
-	name='File Header - Scripts',
-	owner='Explosive Gaming',
-	dev='Cooldude2606',
-	description='Just A File Header To Organise Code',
+	name='Factorio Starting Items',
+	owner='Factorio',
+	dev='Factorio Team',
+	description='Items you get when you join the game',
 	factorio_version='0.15.23',
 	show=false
 	}}
 local function credit_loop(reg) for _,cred in pairs(reg) do table.insert(credits,cred) end end
 --Please Only Edit Below This Line-----------------------------------------------------------
-credit_loop(require("Factorio Starting Items"))
+Event.register(defines.events.on_player_created, function(event)
+  local player = game.players[event.player_index]
+	if event.player_index == 1 then
+		player.force.friendly_fire = false
+		player.force.chart(player.surface, {{player.position.x - 400, player.position.y - 400}, {player.position.x + 400, player.position.y + 400}})
+	end
+	if tick_to_min(game.tick) < 60 then
+		player.insert{name="iron-plate", count=8}
+		player.insert{name="pistol", count=1}
+		player.insert{name="firearm-magazine", count=10}
+		player.insert{name="burner-mining-drill", count = 1}
+		player.insert{name="stone-furnace", count = 1}
+	end
+end)
 --Please Only Edit Above This Line-----------------------------------------------------------
 return credits
