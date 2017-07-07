43192d9cfe2cda3203d93a8f40d3fabe3de1f849
Cooldude2606 <cooldude260607@gmail.com>
2017-07-07 21:07:19 +0100

    CORE FILES

--- control.lua
+++ control.lua
@@ -23,20 +23,3 @@ credit_loop(require("locale/StdLib/event"))
 credit_loop(require("locale/file-header"))
 
 Event.register(-1,function() global.credits = credits end)
-
---temp
-ExpGui.add_frame.center('Test','Test','A Simple Gui',5)
-ExpGui.add_frame.tab('testing','Long Tab','Just Like any other',5,'Test',function(player,tab) for i=0,100 do tab.add{type='label',name=i,caption=player.name} end end)
-ExpGui.add_frame.tab('just_a_tab','A Tab','Just Like any other',5,'Test',function(player,tab) tab.add{type='label',name='temp',caption=player.name} end)
-ExpGui.add_frame.left('left_test','Player Name','Just Like any other',5,true,function(player,frame) for i=0,10 do frame.add{type='label',name=i,caption=player.name..'\n'} end end)
-ExpGui.add_input.button('make_message','Messgaes','Lots of messages',function() for i = 1,10 do ExpGui.draw_frame.popup('Message',{'Just A Message',i}) end end)
-ExpGui.add_frame.popup('Message','Message','Just A ToolTip',5,
-	function(player,frame)
-		ExpGui.add_input.draw_button(frame,'make_message') end,
-	function(player,frame,args) 
-		frame.add{name='Message',type='label',caption=args[1]..' '..args[2]} 
-end)
-ExpGui.add_frame.tab('player_table','Player Table','Just Like any other',5,'Test',function(player,tab) 
-	ExpGui.player_table.draw_filters(player,tab,{'player_name','online_time','rank'})
-	ExpGui.player_table.draw(player,tab,{{'online',true}},tab)
-end)
