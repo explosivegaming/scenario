6827bf7300892c8ecb007278d440ca7084686688
Cooldude2606 <cooldude260607@gmail.com>
2017-07-19 20:39:24 +0100

    Commands now in global list

--- locale/ExpGaming-Core/ExpGaming - Command Maker.lua
+++ locale/ExpGaming-Core/ExpGaming - Command Maker.lua
@@ -76,6 +76,7 @@ function load_command(command)
 	end)
 end
 
+Event.register(-1,function() global.commands = Exp_commands end)
 Event.register(defines.events.on_player_joined_game,function() for _,command in pairs(Exp_commands) do load_command(command) end end)
 --Please Only Edit Above This Line-----------------------------------------------------------
 return credits
