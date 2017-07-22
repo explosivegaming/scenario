8ed8177ba00ed5de8654ac07d2064fb01a015c4d
Cooldude2606 <cooldude260607@gmail.com>
2017-07-22 21:23:16 +0100

    Kill now works

--- locale/ExpGaming-Addons/Commands/ExpGaming - Kill.lua
+++ locale/ExpGaming-Addons/Commands/ExpGaming - Kill.lua
@@ -21,17 +21,17 @@ local function credit_loop(reg) for _,cred in pairs(reg) do table.insert(credits
 define_command('kill','Used to kill a player, you can always kill your self!',{'player'},'Guest',function(player,event,args)
     if player == '<server>' then
         local kill_player = game.players[args[1]]
+        if not kill_player then print('Invaild Player Name,'..args[1]..', try using tab key to auto-complete the name') return end
+        if kill_player.connected then else print('Invaild Player, player is not online') return end
+        if kill_player.character then kill_player.character.die() else print('Invaild Player, their are already dead') return  end
+        print('Command Complete')
+    else
+        local kill_player = game.players[args[1]]
         if not kill_player then player.print('Invaild Player Name,'..args[1]..', try using tab key to auto-complete the name') return end
         if kill_player == player or get_rank(player).power < get_rank(kill_player).power and get_rank(player).power <= string_to_rank('Mod').power then
             if kill_player.connected then else player.print('Invaild Player, player is not online') return end
             if kill_player.character then kill_player.character.die() else player.print('Invaild Player, their are already dead') return end
         else player.print('401 - Unauthorized: Access is denied due to invalid credentials') end
-    else
-        local kill_player = game.players[args[1]]
-        if not kill_player then print('Invaild Player Name,'..args[1]..', try using tab key to auto-complete the name') return end
-        if kill_player.connected then else print('Invaild Player, player is not online') return end
-        if kill_player.character then kill_player.character.die() else print('Invaild Player, their are already dead') return  end
-        print('Command Complete')
     end
 end)
 --Please Only Edit Above This Line-----------------------------------------------------------
