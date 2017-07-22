8448313ac48c27894a35eb4ecaa4a569f884ede0
Cooldude2606 <cooldude260607@gmail.com>
2017-07-22 22:28:01 +0100

    Bug Fixes

--- locale/ExpGaming-Addons/ExpGaming - Temp Ban.lua
+++ locale/ExpGaming-Addons/ExpGaming - Temp Ban.lua
@@ -23,31 +23,35 @@ define_command('temp-ban','Temp ban a player untill the next reset, can not be u
         local jail_player = game.players[args[1]]
         if not jail_player then print('Invaild Player Name,'..args[1]..', try using tab key to auto-complete the name') return end
         give_rank(jail_player,'Jail')
-        table.insert(global.temp_bans,{jail_player.name,player,table.concat(args,' ',2)})
+        table.insert(global.temp_bans,{jail_player.name,player,table.concat(args,' ',2),jail_player})
         ExpGui.draw_frame.popup('temp_ban',{player,table.concat(args,' ',2)})
-        rank_print(jail_player..' has been temp banned by '..player..' reason: '..table.concat(args,' ',2),'Guest')
+        rank_print(jail_player.name..' has been temp banned by '..player..' reason: '..table.concat(args,' ',2),'Guest')
     else
         local jail_player = game.players[args[1]]
         if not jail_player then player.print('Invaild Player Name,'..args[1]..', try using tab key to auto-complete the name') return end
-        if get_rank(player).power > get_rank(player).power or get_rank(player).power > string_to_rank('Mod').power then player.print('401 - Unauthorized: Access is denied due to invalid credentials') return end
-        give_rank(jail_player,'Jail')
-        global.temp_bans[jail_player.name] = {player.name,table.concat(args,' ',2)}
-        ExpGui.draw_frame.popup('temp_ban',{player.name,table.concat(args,' ',2)})
-        rank_print(jail_player..' has been temp banned by '..player.name..' reason: '..table.concat(args,' ',2),'Guest')
+        if get_rank(player).power < get_rank(jail_player).power and get_rank(player).power <= string_to_rank('Mod').power then
+            give_rank(jail_player,'Jail',player)
+            global.temp_bans[jail_player.name] = {player.name,table.concat(args,' ',2)}
+            ExpGui.draw_frame.popup('temp_ban',{player.name,table.concat(args,' ',2),jail_player})
+            rank_print(jail_player.name..' has been temp banned by '..player.name..' reason: '..table.concat(args,' ',2),'Guest')
+        else player.print('401 - Unauthorized: Access is denied due to invalid credentials') return end
     end
 end)
 
 ExpGui.add_frame.popup('temp_ban','Temp Ban','You have been baned till next reset',nil,nil,function(player,frame,args)
-    frame.add{name='title',type='label',caption='You Have Been Temp Banned',style="caption_label_style"}
-    frame.add{name='info',type='label',caption='This can not be revert, you will be unbaned on the next map reset'}
-    frame.add{name='by_player',type='label',caption='You were banned by '..args[1]}
-    frame.add{name='reason',type='label',caption='Reason: '..args[2]}
+    if player == args[3] then
+        frame.add{name='title',type='label',caption='You Have Been Temp Banned',style="caption_label_style"}
+        frame.add{name='info',type='label',caption='This can not be revert, you will be unbaned on the next map reset'}
+        frame.add{name='by_player',type='label',caption='You were banned by '..args[1]}
+        frame.add{name='reason',type='label',caption='Reason: '..args[2]}
+    else frame.parent.destroy()
+    end
 end)
 
 Event.register(defines.events.on_player_joined_game,function(event)
     local player = game.players[event.player_index]
     if global.temp_bans[player.name] then
-        ExpGui.draw_frame.popup('temp_ban',{global.temp_bans[player.name][1],global.temp_bans[player.name][2]})
+        ExpGui.draw_frame.popup('temp_ban',{global.temp_bans[player.name][1],global.temp_bans[player.name][2],player})
     end
 end)
 
