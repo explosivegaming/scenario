7e1aa06a0e69ad55e781622d172d13b104bc11ef
Cooldude2606 <cooldude260607@gmail.com>
2017-07-23 21:05:53 +0100

    Adding Cheat Mode

--- locale/ExpGaming-Addons/Commands/ExpGaming - Cheat Mode.lua
+++ locale/ExpGaming-Addons/Commands/ExpGaming - Cheat Mode.lua
@@ -9,20 +9,26 @@ Discord: https://discord.gg/XSsBV6b
 The credit below may be used by another script do not remove.
 ]]
 local credits = {{
-	name='File Header - ExpGaming-Addons',
+	name='Cheat Command',
 	owner='Explosive Gaming',
 	dev='Cooldude2606',
-	description='Just A File Header To Organise Code',
+	description='Only For the best of the lazyest of the best',
 	factorio_version='0.15.23',
-	show=false
+	show=true
 	}}
 local function credit_loop(reg) for _,cred in pairs(reg) do table.insert(credits,cred) end end
 --Please Only Edit Below This Line-----------------------------------------------------------
-credit_loop(require("ExpGaming - Repair"))
-credit_loop(require("ExpGaming - TP"))
-credit_loop(require("ExpGaming - Tp All"))
-credit_loop(require("ExpGaming - Kill"))
-credit_loop(require("ExpGaming - Tags"))
-credit_loop(require("ExpGaming - Cheat Mode"))
+define_command('cheat-mode','Toggle Cheat Mode',{'player'},'Dev',function(player,event,args)
+    if player == '<server>' then
+        local p = game.players[args[1]]
+        if not p then print('Invaild Player Name,'..args[1]..', try using tab key to auto-complete the name') return end
+        p.cheat_mode = not p.cheat_mode
+        print('Command Complete')
+    else
+        local p = game.players[args[1]]
+        if not p then player.print('Invaild Player Name,'..args[1]..', try using tab key to auto-complete the name') return end
+        p.cheat_mode = not p.cheat_mode
+    end
+end)
 --Please Only Edit Above This Line-----------------------------------------------------------
 return credits
