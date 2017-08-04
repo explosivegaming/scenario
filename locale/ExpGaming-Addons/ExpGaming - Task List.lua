6c9bdce81a400c2a854d8f8acb3b8254d3c083e4
Cooldude2606 <cooldude260607@gmail.com>
2017-08-04 21:54:30 +0100

    Task List Now Works

--- locale/ExpGaming-Addons/ExpGaming - Task List.lua
+++ locale/ExpGaming-Addons/ExpGaming - Task List.lua
@@ -19,12 +19,9 @@ local credits = {{
 local function credit_loop(reg) for _,cred in pairs(reg) do table.insert(credits,cred) end end
 --Please Only Edit Below This Line-----------------------------------------------------------
 local function draw_standard(player,frame)
-	game.print(game.tick..' Standard Mode')
-	game.print(game.tick..' '..table.to_string(global.tasks))
 	frame.clear()
 	local table = frame.add{type='table',name='task_table',colspan=2}
-	game.print(game.tick..' Global Read')
-	for n,task in pairs(global.tasks.global) do
+	for n,task in pairs(global.tasks.server) do
 		table.add{type='label',name=n..'_number',caption=n..'.'}
 		table.add{type='label',name=n..'_task',caption=task}
 	end
@@ -32,11 +29,8 @@ local function draw_standard(player,frame)
 end
 
 local function draw_edit_mode(player,frame)
-	game.print(game.tick..' Edit Mode')
-	game.print(game.tick..' '..table.to_string(global.tasks))
 	frame.clear()
 	local table = frame.add{type='table',name='task_table',colspan=3}
-	game.print(game.tick..' Local Read')
 	for n,task in pairs(global.tasks.players[player.index]) do
 		table.add{type='label',name=n..'_number',caption=n..'.'}
 		table.add{type='text-box',name=n..'_task',text=task}.style.minimal_width=250
@@ -50,18 +44,15 @@ local function draw_edit_mode(player,frame)
 end
 
 ExpGui.add_input.button('add_task','Add','Add a new task',function(player,element)
-	game.print(game.tick..' Local Edit')
-	game.print(game.tick..' Before: '..table.to_string(global.tasks))
+	for n,task in pairs(global.tasks.players[player.index]) do global.tasks.players[player.index][n] = element.parent.parent.task_table[n..'_task'].text end
 	table.insert(global.tasks.players[player.index],'')
-	game.print(game.tick..' After: '..table.to_string(global.tasks))
 	draw_edit_mode(player,element.parent.parent)
 end)
 
 ExpGui.add_input.button('remove','X','Remove this task',function(player,element)
-	game.print(game.tick..' Local Edit')
+	for n,task in pairs(global.tasks.players[player.index]) do global.tasks.players[player.index][n] = element.parent.parent.parent.task_table[n..'_task'].text end
 	local n = tonumber(element.parent.name:match('%d+'))
 	table.remove(global.tasks.players[player.index],n)
-	game.print(game.tick..' '..table.to_string(global.tasks))
 	draw_edit_mode(player,element.parent.parent.parent)
 end)
 
@@ -70,22 +61,17 @@ ExpGui.add_input.button('exit','Exit','Returns to stanard view without saving',f
 end)
 
 ExpGui.add_input.button('save','Save','Press to save any changes (will update for other users)',function(player,element)
-	game.print(game.tick..' Local Read')
-	for n,_ in pairs(global.tasks.players[player.index]) do
-		game.print(game.tick..' Local Edit') 
+	local temp_list = {}
+	for n,task in pairs(global.tasks.players[player.index]) do
 		global.tasks.players[player.index][n] = element.parent.parent.task_table[n..'_task'].text
-		game.print(game.tick..' '..table.to_string(global.tasks))
+		if global.tasks.players[player.index][n]:match('%S') then table.insert(temp_list,global.tasks.players[player.index][n]) end
 	end
-	game.print(game.tick..' Global Edit')
-	global.tasks.global = global.tasks.players[player.index]
-	game.print(game.tick..' '..table.to_string(global.tasks))
+	global.tasks.server = {table.unpack(temp_list)}
 	for _,p in pairs(game.connected_players) do ExpGui.draw_frame.left(player,'task_list',true) end
 end)
 
 ExpGui.add_input.button('enter_edit','Press To Edit','Press to edit the current tasks',function(player,element)
-	game.print(game.tick..' Local Set')
-	global.tasks.players[player.index] = global.tasks.global
-	game.print(game.tick..' '..table.to_string(global.tasks))
+	global.tasks.players[player.index] = {table.unpack(global.tasks.server)}
 	draw_edit_mode(player,element.parent)
 end)
 
@@ -95,6 +81,6 @@ ExpGui.add_frame.left('task_list','item/inserter','Toggle Task List','Guest',tru
 end)
 
 Event.register(Event.rank_change,function(event) for _,player in pairs(game.connected_players) do ExpGui.draw_frame.left(player,'task_list',true) end end)
-Event.register(-1,function() global.tasks = {global={'test','test2','test3'},players={}} end)
+Event.register(-1,function() global.tasks = {server={'test','test2','test3'},players={}} end)
 --Please Only Edit Above This Line-----------------------------------------------------------
 return credits
