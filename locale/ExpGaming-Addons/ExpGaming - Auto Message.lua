c41797aa34c1f7debd5a1b978be2061ed77ff9f8
Cooldude2606 <cooldude260607@gmail.com>
2017-10-17 19:27:08 +0100

    auto message clean

--- locale/ExpGaming-Addons/ExpGaming - Auto Message.lua
+++ locale/ExpGaming-Addons/ExpGaming - Auto Message.lua
@@ -5,41 +5,17 @@ This file can be used with permission but this and the credit below must remain 
 Contact a member of management on our discord to seek permission to use our code.
 Any changes that you may make to the code are yours but that does not make the script yours.
 Discord: https://discord.gg/XSsBV6b
-
-The credit below may be used by another script do not remove.
 ]]
-local credits = {{
-	name='Auto Message',
-	owner='Explosive Gaming',
-	dev='Cooldude2606',
-	description='A simple message that is showen in chat every gui update',
-	factorio_version='0.15.23',
-	show=true
-	}}
-
 --Please Only Edit Below This Line-----------------------------------------------------------
-local low_ranks_only={
-    'Please join us on:',
-    'Discord: https://discord.gg/RPCxzgt',
-    'Forum: explosivegaming.nl',
-    'Steam: http://steamcommunity.com/groups/tntexplosivegaming',
-    'To see these links again goto: Info > Links',
-    'We also have some custom commands which you can view in: Info > Commands',
-    'This includes useful commands such as /tag and /report',
-    'Do /help <command> for more info'
-}
-
 function auto_message(event)
 	if event.player_loop_index < event.players_online then return end
 	local low_rank = 'Regular'
 	local high_rank = 'Owner'
-	server.queue_callback(rank_print,{'There are '..#game.connected_players..' players online',high_rank,true})
-	server.queue_callback(rank_print,{'This map has been on for '..tick_to_display_format(game.tick),high_rank,true})
-	for _,message in pairs(low_ranks_only) do
-		server.queue_callback(rank_print,{message,low_rank,true})
+	server.queue_callback(rank_print,{{'auto-message.players-online',#game.connected_players},high_rank,true})
+	server.queue_callback(rank_print,{{'auto-message.map-time',tick_to_display_format(game.tick)},high_rank,true})
+	for i = 1,8 do -- if you know a way to set this limit from the locale file please tell me :)
+		server.queue_callback(rank_print,{{'auto-message.line-'..tostring(i)},low_rank,true})
 	end
 end
 
 Event.register(Event.gui_update,auto_message)
-
-
