5a27280dc9a2885a9cdf3beeedcf9c3caab970f7
Cooldude2606 <cooldude260607@gmail.com>
2017-10-19 16:59:15 +0100

    Discord link edit

--- locale/ExpGaming-Addons/ExpGaming - Announcements.lua
+++ locale/ExpGaming-Addons/ExpGaming - Announcements.lua
@@ -4,7 +4,7 @@ Explosive Gaming
 This file can be used with permission but this and the credit below must remain in the file.
 Contact a member of management on our discord to seek permission to use our code.
 Any changes that you may make to the code are yours but that does not make the script yours.
-Discord: https://discord.gg/XSsBV6b
+Discord: https://discord.gg/r6dC2uK
 ]]
 --Please Only Edit Below This Line-----------------------------------------------------------
 --this function can be called at any time to make an announcement
