1aeaa6033063e7c6b4c6fe2d2abd04655a94e14d
Cooldude2606 <cooldude260607@gmail.com>
2017-10-21 22:47:26 +0100

    Tree remover clean

--- locale/ExpGaming-Addons/ExpGaming - Tree Remover.lua
+++ locale/ExpGaming-Addons/ExpGaming - Tree Remover.lua
@@ -5,51 +5,41 @@ This file can be used with permission but this and the credit below must remain 
 Contact a member of management on our discord to seek permission to use our code.
 Any changes that you may make to the code are yours but that does not make the script yours.
 Discord: https://discord.gg/r6dC2uK
-
-The credit below may be used by another script do not remove.
 ]]
-local credits = {{
-	name='Tree Remover',
-	owner='Explosive Gaming',
-	dev='Cooldude2606',
-	description='Removes trees and stone with use of deconstruction planer',
-	factorio_version='0.15.23',
-	show=true
-	}}
-
 --Please Only Edit Below This Line-----------------------------------------------------------
 Event.register(defines.events.on_marked_for_deconstruction, function(event)
+	-- varible setup
 	if not event.entity.valid then return end
 	local player = game.players[event.player_index]
 	local entity = event.entity
-	-- sets up the temp var value to be used in later sudo functions
-	local tree_remover = nil
-	if not server.get_uuid_data(player.name..'_tree_remover') then
+	-- get the uuid and data
+	local tree_remover_uuid = server.get_uuid(player.name..'_tree_remover')
+	if not server.get_uuid_data(tree_remover_uuid) then
 		tree_remover = server.queue_callback(function(player)
 			if not rank_allowed(ranking.get_player_rank(player),'decon') then return 1
 			elseif rank_allowed(ranking.get_player_rank(player),'tree_remover') then return 2 
 			else return 0 end
-		end,{player},player.name..'_tree_remover')
-	else tree_remover = server.get_uuid(player.name..'_tree_remover') end
-	-- using the temp var stored in tree_remover sudo will take diffrent effects while only running the test once
-	server.queue_callback(function(entity,tree_remover)
+		end,{player},tree_remover_uuid)
+	end
+	-- run the main tree remover and get the uuid
+	-- the printed varible is recicive as a way to chain the commands together
+	local printed_uuid = server.get_uuid(player.name..'_tree_remover_printed')
+	server.queue_callback(function(entity,tree_remover,printed)
 		if not entity.valid then return end
-		local result = tree_remover.data[1]
-		server.refresh_uuid(tree_remover.temp_var_name)
-		local printed = nil; if type(server.get_uuid_data(player.name..'_tree_remover_printed')) == 'table' then printed = server.get_uuid_data(player.name..'_tree_remover_printed')[1] end
-		if result == 1 and entity.last_user then
+		-- tree remover is the function above either 0,1,2
+		if tree_remover == 1 and entity.last_user then
 			entity.cancel_deconstruction('player')
+			-- if it has not already printed then print
 			if printed ~= true then
 				debug_write({'TREE-REMOVER'},printed)
-				player.print('You are not allowed to do this yet, You require the Regular rank, you must play for at least 3 hours')
-				ranking.rank_print(player.name..' tryed to deconstruced something.')
+				player.print{'tree-remover.player-print'}
+				ranking.rank_print({'tree-remover.rank-print',player.name})
 			end return true
-		elseif result == 2 and not entity.last_user then
+		elseif tree_remover == 2 and not entity.last_user then
 			entity.destroy()
 		end
 		if printed == true then return true end
-	end,{entity,tree_remover},player.name..'_tree_remover_printed')
-
+	end,{entity,tree_remover_uuid,printed_uuid},printed_uuid)
 end)
 
 
