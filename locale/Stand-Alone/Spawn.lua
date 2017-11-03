5c92536998d65f83a3e41d5537a49b1c18fba078
Cooldude2606 <cooldude260607@gmail.com>
2017-11-03 16:57:53 +0000

    Allowed entitys

--- locale/Stand-Alone/Spawn.lua
+++ locale/Stand-Alone/Spawn.lua
@@ -13,17 +13,25 @@ note for positions
 {-1,0} {0,0} {1,0}
 {-1,1} {0,1} {1,1}
 --]]
-local positions = {
+local tile_positions = {
     {0,0},{-1,-1},{1,-1},{-1,1},{1,1}
+}
+
+local entitys = {
+    {'iron-chest',-2,-2},{'iron-chest',2,2},{'iron-chest',2,-2},{'iron-chest',-2,2}
 }
 
 Event.register(defines.events.on_player_created, function(event)
     if event.player_index == 1 then
         local offset = game.players[event.player_index].character.position
         local tiles = {}
-        for _,position in pairs(positions) do
+        for _,position in pairs(tile_positions) do
             table.insert(tiles,{name='stone-path',position={position[1]+offset.x,position[2]+offset.y}})
         end
         game.players[event.player_index].surface.set_tiles(tiles)
+        for _,entity in pairs(entitys) do
+            local entity = game.players[event.player_index].surface.create_entity{name=entity[1],position={entity[2]+offset.x,entity[3]+offset.y},force='neutral'}
+            entity.destructible = false; entity.health = 0; entity.minable = false; entity.rotatable = false
+        end
     end
 end)
