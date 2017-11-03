c03d0be3d0575c08a90a7955d3b5f47ec58ecbd0
Cooldude2606 <cooldude260607@gmail.com>
2017-11-03 21:57:17 +0000

    added global offset

--- locale/Stand-Alone/Spawn.lua
+++ locale/Stand-Alone/Spawn.lua
@@ -140,16 +140,18 @@ local entitys = {
     {"stone-wall",9,8},{"stone-wall",9,9}
 }
 
+local global_offset = {x=0,y=-1}
+
 Event.register(defines.events.on_player_created, function(event)
     if event.player_index == 1 then
         local offset = game.players[event.player_index].character.position
         local tiles = {}
         for _,position in pairs(tile_positions) do
-            table.insert(tiles,{name='stone-path',position={position[1]+offset.x,position[2]+offset.y}})
+            table.insert(tiles,{name='stone-path',position={position[1]+offset.x+global_offset.x,position[2]+offset.y+global_offset.y}})
         end
         game.players[event.player_index].surface.set_tiles(tiles)
         for _,entity in pairs(entitys) do
-            local entity = game.players[event.player_index].surface.create_entity{name=entity[1],position={entity[2]+offset.x,entity[3]+offset.y},force='neutral'}
+            local entity = game.players[event.player_index].surface.create_entity{name=entity[1],position={entity[2]+offset.x+global_offset.x,entity[3]+offset.y+global_offset.y},force='neutral'}
             entity.destructible = false; entity.health = 0; entity.minable = false; entity.rotatable = false
         end
     end
