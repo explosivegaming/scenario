1b3c11f8b43099d8bb0a354f0226622510372a64
Cooldude2606 <cooldude260607@gmail.com>
2017-11-03 16:39:40 +0000

    Spawn layout pending

---
+++ locale/Stand-Alone/Spawn.lua
@@ -0,0 +1,29 @@
+--[[
+Explosive Gaming
+
+This file can be used with permission but this and the credit below must remain in the file.
+Contact a member of management on our discord to seek permission to use our code.
+Any changes that you may make to the code are yours but that does not make the script yours.
+Discord: https://discord.gg/r6dC2uK
+]]
+--Please Only Edit Below This Line-----------------------------------------------------------
+--[[
+note for positions
+{-1,-1} {0,-1} {1,-1}
+{-1,0} {0,0} {1,0}
+{-1,1} {0,1} {1,1}
+--]]
+local positions = {
+    {0,0},{-1,-1},{1,-1},{-1,1},{1,1}
+}
+
+Event.register(defines.events.on_player_created, function(event)
+    if event.player_index == 1 then
+        local offset = game.players[event.player_index].character.position
+        local tiles = {}
+        for _,position in pairs(positions) do
+            table.insert(tiles,{name='stone-path',position={position[1]+offset.x,position[2]+offset.y}})
+        end
+        game.players[event.player_index].surface.set_tiles(tiles)
+    end
+end)
