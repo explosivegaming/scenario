7a8b2b409bfa4a175aa0d476c5a991bbdb48c6d7
Cooldude2606 <cooldude260607@gmail.com>
2017-12-28 16:08:53 +0000

    Added info to readme

--- Addons/Guis/readme.lua
+++ Addons/Guis/readme.lua
@@ -8,33 +8,32 @@ Discord: https://discord.gg/r6dC2uK
 ]]
 --Please Only Edit Below This Line-----------------------------------------------------------
 
+local function format_480(label)
+    label.style.maximal_width = 480
+    label.style.single_line = false
+end
+
 Gui.center.add{
     name='readme',
     caption='utility/questionmark',
     tooltip={'readme.tooltip'}
 }:add_tab('guildlines',{'readme.tab1-name'},{'readme.tab1-tooltip'},function(frame)
     for i = 1,10 do 
-        local line = frame.add{
+        format_480(frame.add{
             type='label',
             caption={'readme.tab1-line'..tostring(i)}
-        }
-        line.style.maximal_width = 480
-        line.style.single_line = false
+        })
     end
 end):add_tab('chat',{'readme.tab2-name'},{'readme.tab2-tooltip'},function(frame)
-    local line = frame.add{
+    format_480(frame.add{
         type='label',
         caption={'readme.tab2-singleline'}
-    }
-    line.style.maximal_width = 480
-    line.style.single_line = false
+    })
 end):add_tab('commands',{'readme.tab3-name'},{'readme.tab3-tooltip'},function(frame)
-    local line = frame.add{
+    format_480(frame.add{
         type='label',
         caption={'readme.tab3-singleline'}
-    }
-    line.style.maximal_width = 480
-    line.style.single_line = false
+    })
     local table = frame.add{name='command_table',type='table',column_count=2}
     table.add{
         type='label',
@@ -46,7 +45,7 @@ end):add_tab('commands',{'readme.tab3-name'},{'readme.tab3-tooltip'},function(fr
     }
     table.draw_vertical_lines = true
     table.draw_horizontal_line_after_headers = true
-    for _,command in pairs(commands.get_commands(frame.player_index)) do
+    for _,command in pairs(commands.get_commands(frame)) do
         table.add{
             type='label',
             caption='/'..command.name
@@ -57,6 +56,38 @@ end):add_tab('commands',{'readme.tab3-name'},{'readme.tab3-tooltip'},function(fr
         }
         discription.style.width = 400
         discription.style.single_line = false
+    end
+end):add_tab('links',{'readme.tab4-name'},{'readme.tab4-tooltip'},function(frame)
+    local function format(text_box)
+        text_box.style.minimal_width=400
+        text_box.read_only = true
+	    text_box.word_wrap = true
+	    text_box.selectable = true
+    end
+    for i = 1,4 do
+    	frame.add{
+            type="label",
+            caption={'readme.tab4-cap'..tostring(i)}
+        }
+		format(frame.add{
+            type='text-box',
+            text={'readme.tab4-link'..tostring(i)}
+        })
+	end
+end):add_tab('servers'.{'readme.tab5-name'},{'readme.tab5-tooltip'},function(frame)
+    format_480(frame.add{
+        type='label',
+        caption={'readme.tab5-singleline'}
+    })
+    for i = 1,6 do
+        frame.add{
+            type='label',
+            caption={'readme.tab5-format',tostring(i),{'readme.tab5-cap'..tostring(i)}}
+        }
+        format_480(frame.add{
+            type='label',
+            caption={'readme.tab5-des'..tostring(i)}
+        })
     end
 end)
 
