177553d906cdac7986231651ba1a79ac298213db
Cooldude2606 <cooldude260607@gmail.com>
2018-03-16 16:02:16 +0000

    Small Edit To Corpse Marker

--- StandAlone/body-marker.lua
+++ StandAlone/body-marker.lua
@@ -2,9 +2,13 @@
 
 Event.register(defines.events.on_player_died, function(event)
     local player = game.players[event.player_index]
+    local o = player.position
+    local entity = player.surface.find_entities_filtered{area={{o.x-1,o.y-1},{o.x+1,o.y+1}},name='character-corpse'}
+    table.insert(entity,nil)
     local tag = player.force.add_chart_tag(player.surface,{
         position=player.position,
-        text='Death: '..player.name..' ('..tick_to_display_format(event.tick)..')'
+        text='Death: '..player.name..' ('..tick_to_display_format(event.tick)..')',
+        target=entity[1]
     })
     if not global.corpses then global.corpses = {} end
     table.insert(global.corpses,tag)
@@ -15,11 +19,6 @@ Event.register(defines.events.on_tick, function(event)
     if not global.corpses then global.corpses = {} end
     for key,tag in pairs(global.corpses) do
         if not tag.valid then global.corpses[key] = nil return end
-        if not tag.target then 
-            local o = tag.position 
-            local entity = tag.surface.find_entities_filtered{area={{o.x-1,o.y-1},{o.x+1,o.y+1}},name='character-corpse'}
-            if entity[1] then tag.target = entity[1] end
-        end
         if not tag.target or not tag.target.valid then tag.destroy() global.corpses[key] = nil return end
     end
 end)
