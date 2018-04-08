1aa2f67ffa813852867d2cadf78054376629a846
Cooldude2606 <cooldude260607@gmail.com>
2018-04-08 17:04:08 +0100

    Added More Buttons To Server Info

--- Addons/Guis/readme.lua
+++ Addons/Guis/readme.lua
@@ -121,3 +121,11 @@ end)
 Sync.add_to_gui(Gui.inputs.add_button('readme-sync-guildlines','View Guildlines','View the guildlines in the readme',function(player,element)
     Gui.center.open_tab(player,'readme','guildlines')
 end))
+
+Sync.add_to_gui(Gui.inputs.add_button('readme-sync-links','View Other Links','View the links in the readme',function(player,element)
+    Gui.center.open_tab(player,'readme','links')
+end))
+
+Sync.add_to_gui(Gui.inputs.add_button('readme-sync-rules','View All Rules','View the all rules in the readme',function(player,element)
+    Gui.center.open_tab(player,'readme','rules')
+end))
