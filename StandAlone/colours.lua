241b0e0e767c6144d55467acfba544f1421f5fab
Cooldude2606 <cooldude260607@gmail.com>
2018-04-13 21:34:03 +0100

    No More White Player Color

--- StandAlone/colours.lua
+++ StandAlone/colours.lua
@@ -13,7 +13,8 @@ Event.register(defines.events.on_player_created, function(event)
     local player = game.players[event.player_index]
 	local colours = table.keys(defines.color)
 	player.color = defines.color.black
-	while player.color.r == defines.color.black.r and player.color.g == defines.color.black.g and player.color.b == defines.color.black.b do
+	while player.color.r == defines.color.black.r and player.color.g == defines.color.black.g and player.color.b == defines.color.black.b 
+	or player.color.r == defines.color.white.r and player.color.g == defines.color.white.g and player.color.b == defines.color.white.b  do
 		player.color = defines.color[colours[math.random(#colours)]]
 		if default_colours[player.name] then
 			local c = default_colours[player.name]
