f0051d2ce983fdb2f0743063554c021836b10365
Cooldude2606 <cooldude260607@gmail.com>
2018-04-20 15:28:08 +0100

    Fixed Bug In Reports.lua

--- Addons/Admin/reports.lua
+++ Addons/Admin/reports.lua
@@ -150,7 +150,7 @@ function Admin.report(player,by_player,reason)
     local player, by_player_name = valid_players(player,by_player)
     if not player or Ranking.get_rank(player):allowed('no-report') then return end
     if Admin.is_banned(by_player) or Ranking.get_group(by_player).name == 'Jail' then return end
-    if rank:allowed('varified') then 
+    if Ranking.get_rank(by_player):allowed('varified') then 
         _reports().varified[player.name] = _reports().varified[player.name] or {} 
         local reports = _reports().varified[player.name]
         for _,value in pairs(reports) do
