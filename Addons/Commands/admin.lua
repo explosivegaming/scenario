12e0f3adc789e1e4c924efb504893ccb51f7cfe4
Cooldude2606 <cooldude260607@gmail.com>
2018-04-27 17:14:18 +0100

    Added cant-report-ban to admin commands

--- Addons/Commands/admin.lua
+++ Addons/Commands/admin.lua
@@ -13,7 +13,8 @@ commands.add_command('report', 'Reports a player.', {'player','reason',true}, fu
     local player = Game.get_player(args.player)
     local reason = args.reason
     if not player then player_return({'commands.invalid-player',args.player}) return commands.error end
-    if Ranking.get_rank(player):allowed('no-report') or Admin.is_banned(player) then player_return({'reports.cant-report',args.player}) return commands.error end
+    if Admin.is_banned(player) then player_return({'reports.cant-report-ban',args.player}) return commands.error end
+    if Ranking.get_rank(player):allowed('no-report') then player_return({'reports.cant-report',args.player}) return commands.error end
     for _,report in pairs(global.addons.reports.reports) do if report[1] == _player.name then player_return({'reports.cant-report',args.player}) return commands.error end end
     for _,report in pairs(global.addons.reports.varified) do if report[1] == _player.name then player_return({'reports.cant-report',args.player}) return commands.error end end
     Admin.report(player,event.player_index,reason)
@@ -23,7 +24,8 @@ commands.add_command('warn', 'Gives a player a warnings', {'player','reason',tru
     local player = Game.get_player(args.player)
     local reason = args.reason
     if not player then player_return({'commands.invalid-player',args.player}) return commands.error end
-    if Ranking.get_rank(player):allowed('no-report') or Admin.is_banned(player) then player_return({'reports.cant-report',args.player}) return commands.error end
+    if Admin.is_banned(player) then player_return({'reports.cant-report-ban',args.player}) return commands.error end
+    if Ranking.get_rank(player):allowed('no-report') then player_return({'reports.cant-report',args.player}) return commands.error end
     Admin.give_warning(player,event.player_index,reason)
 end)
 
@@ -31,13 +33,15 @@ commands.add_command('jail', 'Jails a player', {'player','reason',true}, functio
     local player = Game.get_player(args.player)
     local reason = args.reason
     if not player then player_return({'commands.invalid-player',args.player}) return commands.error end
-    if Ranking.get_rank(player):allowed('no-report') or Admin.is_banned(player) then player_return({'reports.cant-report',args.player}) return commands.error end
+    if Ranking.get_rank(player):allowed('no-report') then player_return({'reports.cant-report',args.player}) return commands.error end
+    if Admin.is_banned(player) then player_return({'commands.cant-report-ban',args.player}) return commands.error end
     Admin.jail(player,event.player_index,reason)
 end)
 
 commands.add_command('unjail', 'Returns the players old rank', {'player',true}, function(event,args)
     local player = Game.get_player(args.player)
     if not player then player_return({'commands.invalid-player',args.player}) return commands.error end
+    if Admin.is_banned(player) then player_return({'commands.cant-report-ban',args.player}) return commands.error end
     Ranking.revert(player,event.player_index)
 end)
 
@@ -45,25 +49,28 @@ commands.add_command('temp-ban', 'Temporarily ban a player.', {'player','reason'
     local player = Game.get_player(args.player)
     local reason = args.reason
     if not player then player_return({'commands.invalid-player',args.player}) return commands.error end
-    if Admin.is_banned(player) then player_return({'reports.cant-report',args.player}) return commands.error end
+    if Admin.is_banned(player) then player_return({'reports.cant-report-ban',args.player}) return commands.error end
     Admin.temp_ban(player,event.player_index,reason)
 end)
 
 commands.add_command('clear-inv', 'Clears the player\'s invetory', {'player'}, function(event,args)
     local player = Game.get_player(args.player)
     if not player then player_return({'commands.invalid-player',args.player}) return commands.error end
+    if Admin.is_banned(player) then player_return({'reports.cant-report-ban',args.player}) return commands.error end
     Admin.move_inventory(player)
 end)
 
 commands.add_command('clear-warings', 'Clears the warnings on a player.', {'player'}, function(event,args)
     local player = Game.get_player(args.player)
     if not player then player_return({'commands.invalid-player',args.player}) return commands.error end
+    if Admin.is_banned(player) then player_return({'reports.cant-report-ban',args.player}) return commands.error end
     Admin.clear_warings(player,event.player_index)
 end)
 
 commands.add_command('clear-reports', 'Clears the reports on a player.', {'player'}, function(event,args)
     local player = Game.get_player(args.player)
     if not player then player_return({'commands.invalid-player',args.player}) return commands.error end
+    if Admin.is_banned(player) then player_return({'reports.cant-report-ban',args.player}) return commands.error end
     Admin.clear_reports(player,event.player_index)
 end)
 
