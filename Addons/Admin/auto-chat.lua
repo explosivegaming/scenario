db1625a1dac873a38642814c151d112331c293e6
Cooldude2606 <cooldude260607@gmail.com>
2018-05-14 20:22:15 +0100

    Auto Chat Last Fix

--- Addons/Admin/auto-chat.lua
+++ Addons/Admin/auto-chat.lua
@@ -45,7 +45,7 @@ local commands = {
     ['orderpizza']=function(player) Server.new_thread{
         timeout=math.floor(180*(math.random()+0.5)),data={player.name,0}, reopen=true
     }:on_event('timeout',function(self)
-        if self.data[2]==0 then game.print{'chat-bot.message',{'chat-bot.order-pizza-2',self.data[1]}}                     
+        if self.data[2]==0 then game.print{'chat-bot.message',{'chat-bot.order-pizza-2',self.data[1]}}                
         elseif self.data[2]==1 then game.print{'chat-bot.message',{'chat-bot.order-pizza-3',self.data[1]}} self.reopen = false
         end
         self.data[2]=self.data[2]+1
