db3515e122f3b6c57655abc8aa1aa1b670f44966
sakamakj <38378426+sakamakj@users.noreply.github.com>
2018-05-14 20:54:03 +0200

    my messing around with the chat box (#45)
    * Create base-damage.cfg
    
    * Update base-damage.cfg
    
    * spellfix
    
    grievers changed to griefers
    
    * Create admin.cfg
    
    needs editing
    
    * Create announcements.cfg
    
    * Update announcements.cfg
    
    * needs translatoin
    
    * needs translation
    
    still in English
    
    * needs translation
    
    * needs translation
    
    * Create inventory-search.cfg
    
    * translation fixed
    
    not much more to write
    
    * translation fixed
    
    * needs translation
    
    * needs translation
    
    * needs translation
    
    * needs translation
    
    * needs translation
    
    * needs translation
    
    * needs translation
    
    * needs translation
    
    * needs translation
    
    * translation fixed
    
    * unfinishied
    
    * Translation mostly fixed
    
    Couldnt find the right words to correctly translate the emotions and genuine feelings behind "loops" and #fear the aids", they are so genuine as they are now.
    
    * translation fixed
    
    * translation fixed
    
    * translation fixed
    
    * translation fixed
    
    * translation fixed
    
    * translation fixed
    
    * hopefully correctly translated (read my comment)
    
    not sure if "not enough credentials" is correctly translated. I supposed you meant not enough security clearance or somthing like that. I could have written (Access denied due to invalid rank= Tillgång nekas på grund av otillräcklig rang)
    
    * translation fixed
    
    * translation fixed
    
    * translation fixed
    
    * grammar fix
    
    * grammer fix
    
    Yeah, I copied the grammar error from original English file and made a similiar error in the swedish version.
    
    * spell fix
    
    * better grammar
    
    * estetical language change
    
    * added order pizza
    
    * order pizza
    
    * fix
    
    * Update chat-bot.cfg

--- Addons/Admin/auto-chat.lua
+++ Addons/Admin/auto-chat.lua
@@ -41,7 +41,15 @@ local commands = {
     ['magic']={'chat-bot.magic'},
     ['aids']={'chat-bot.aids'},
     ['riot']={'chat-bot.riot'},
-    ['lenny']={'chat-bot.lenny'},
+    ['lenny']={'chat-bot.lenny'}
+    ['orderpizza']=function(player) Server.new_thread{
+        timeout=math.floor(180*(math.random()+0.5)),data={player.name,0}, reopen=true
+    }:on_event('timeout',function(self)
+        if self.data[2]==0 then game.print{'chat-bot.message',{'chat-bot.order-pizza-2',self.data[1]}}                     
+        elseif self.data[2]==1 then game.print{'chat-bot.message',{'chat-bot.order-pizza-3',self.data[1]}} self.reopen = false
+        end
+        self.data[2]=sel.data[2] + 1
+    end):open() return {'chat-bot.order-pizza-1'} end,
     ['maketea']=function(player) Server.new_thread{
         timeout=math.floor(180*(math.random()+0.5)),data=player.name
     }:on_event('timeout',function(self)
