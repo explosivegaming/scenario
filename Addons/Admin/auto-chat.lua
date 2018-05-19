2573500c0863298aa96787032972a7dbfef834c9
sakamakj <38378426+sakamakj@users.noreply.github.com>
2018-05-19 16:41:51 +0200

    !make cocktail

--- Addons/Admin/auto-chat.lua
+++ Addons/Admin/auto-chat.lua
@@ -60,6 +60,14 @@ local commands = {
         end
         self.data[2]=self.data[2]+1
     end):open() return {'chat-bot.get-snaps-1'} end,
+    ['makecocktail']=function(player) Server.new_thread{
+        timeout=math.floor(180*(math.random()+0.5)),data={player.name,0}, reopen=true
+    }:on_event('timeout',function(self)
+        if self.data[2]==0 then game.print{'chat-bot.message',{'chat-bot.get-cocktail-2',self.data[1]}}                
+        elseif self.data[2]==1 then game.print{'chat-bot.message',{'chat-bot.get-cocktail-3',self.data[1]}} self.reopen = false
+        end
+        self.data[2]=self.data[2]+1
+    end):open() return {'chat-bot.get-cocktail-1'} end,
 
 	['makecoffee']=function(player) Server.new_thread{
         timeout=math.floor(180*(math.random()+0.5)),data=player.name
