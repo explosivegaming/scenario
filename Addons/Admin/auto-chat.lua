08979ae0ae27aa96f6cb750c5fb6fe387062afdf
sakamakj <38378426+sakamakj@users.noreply.github.com>
2018-05-19 02:59:32 +0200

    make coffee

--- Addons/Admin/auto-chat.lua
+++ Addons/Admin/auto-chat.lua
@@ -57,6 +57,11 @@ local commands = {
         if self.data then game.print{'chat-bot.message',{'chat-bot.make-coffee-2',self.data}} end
     end):open() return {'chat-bot.make-coffee-1'} end
 },
+	['makecoffee']=function(player) Server.new_thread{
+        timeout=math.floor(180*(math.random()+0.5)),data=player.name
+    }:on_event('timeout',function(self)
+        if self.data then game.print{'chat-bot.message',{'chat-bot.make-coffee-2',self.data}} end
+    end):open() return {'chat-bot.make-coffee-1'} end,
     ['maketea']=function(player) Server.new_thread{
         timeout=math.floor(180*(math.random()+0.5)),data=player.name
     }:on_event('timeout',function(self)
