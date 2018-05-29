7584cd9a9a31e4e68b26b9da892d79a61d82049a
sakamakj <38378426+sakamakj@users.noreply.github.com>
2018-05-30 01:29:11 +0200

    added shout, !make popcorn (#62)
    * Create base-damage.cfg
    
    * Update base-damage.cfg
    
    * spellfix
    
    grievers changed to griefers
    
    * Create admin.cfg
    
    needs editing
    
    * Create announcements.cfg
    
    * Update announcements.cfg
    
    * needs translatoin
    
    * needs translation
    
    still in English
    
    * needs translation
    
    * needs translation
    
    * Create inventory-search.cfg
    
    * translation fixed
    
    not much more to write
    
    * translation fixed
    
    * needs translation
    
    * needs translation
    
    * needs translation
    
    * needs translation
    
    * needs translation
    
    * needs translation
    
    * needs translation
    
    * needs translation
    
    * needs translation
    
    * translation fixed
    
    * unfinishied
    
    * Translation mostly fixed
    
    Couldnt find the right words to correctly translate the emotions and genuine feelings behind "loops" and #fear the aids", they are so genuine as they are now.
    
    * translation fixed
    
    * translation fixed
    
    * translation fixed
    
    * translation fixed
    
    * translation fixed
    
    * translation fixed
    
    * hopefully correctly translated (read my comment)
    
    not sure if "not enough credentials" is correctly translated. I supposed you meant not enough security clearance or somthing like that. I could have written (Access denied due to invalid rank= Tillgång nekas på grund av otillräcklig rang)
    
    * translation fixed
    
    * translation fixed
    
    * translation fixed
    
    * grammar fix
    
    * grammer fix
    
    Yeah, I copied the grammar error from original English file and made a similiar error in the swedish version.
    
    * spell fix
    
    * better grammar
    
    * estetical language change
    
    * added order pizza
    
    * order pizza
    
    * fix
    
    * Update chat-bot.cfg
    
    * spelfix
    
    * Update auto-chat.lua
    
    * added make-cofee
    
    * added make cofee
    
    * fix because I cant spell apperantly
    
    * spell fix because I cant spell apperantly
    
    * missing =
    
    * coffee
    
    * make coffee
    
    * make coffee
    
    * food
    
    I find it quite funny that arty would pin that message on discord and it is still there.
    
    * food
    
    I wonder how long that pin will stay about the random food suggestion generator...
    
    * food changed to what to eat
    
    * food changed to whattoeat
    
    * Update chat-bot.cfg
    
    * Coffee on twise
    
    * mead and snaps added
    
    * !mead please
    
    * a fix
    
    * !pass some snaps
    
    * get-cocktail
    
    * !make cocktail
    
    * fix?
    
    * fix?
    
    * rhd (thadius suggestion)
    
    * lhd message added
    
    * Fixed Comment and indentation
    
    * rhd changed from shout to messages
    
    * I am also a lazy bastard
    
    * Update colours.lua
    
    * Not needed in commands if present in messages
    
    * Fixed missing comma
    
    * !make popcorn
    
    need more shouts. Somebody tried this without result so why not.
    
    * get-popcorn added
    
    * addition

--- Addons/Admin/auto-chat.lua
+++ Addons/Admin/auto-chat.lua
@@ -46,6 +46,11 @@ local commands = {
     ['lenny']={'chat-bot.lenny'},
     --Jokes about food and drink
     ['whattoeat']={'chat-bot.food'},
+    ['makepopcorn']=function(player) Server.new_thread{
+        timeout=math.floor(180*(math.random()+0.5)),data=player.name
+    }:on_event('timeout',function(self)
+        if self.data then game.print{'chat-bot.message',{'chat-bot.get-popcorn-2',self.data}} end
+    end):open() return {'chat-bot.get-popcorn-1'} end,    
     ['orderpizza']=function(player) Server.new_thread{
         timeout=math.floor(180*(math.random()+0.5)),data={player.name,0}, reopen=true
     }:on_event('timeout',function(self)
