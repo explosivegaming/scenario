7173e75050ec39eb998739c7d83f2f4214c1a073
sakamakj <sakamakj@gmail.com>
2018-05-29 19:49:31 +0200

    addition

--- StandAlone/colours.lua
+++ StandAlone/colours.lua
@@ -7,16 +7,13 @@ local default_colours = {
 	Cooldude2606={r=57,g=192,b=207},
 	mark9064={r=99,g=0,b=255},
 	eissturm={r=25,g=25,b=112},
-<<<<<<< HEAD
-	Sakama={r=20,g=213,b=80}
-=======
+	Sakama={r=20,g=213,b=80},
 	Sakama={r=20,g=213,b=80},
 	freek18={r=50,g=0,b=255},
 	aldldl={r=0,g=131,b=255},
 	NAD4X4={r=135,g=206,b=250},
 	cydes={r=82,g=249,b=155},
 	UUBlueFire={r=0,g=204,b=255}
->>>>>>> dda4951123290245f13568538bdb3d2988970aa3
 }
 
 Event.register(defines.events.on_player_created, function(event)
