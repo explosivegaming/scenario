a17190d7d084aa72daab8c83856f3cefc7d1d484
Cooldude2606 <cooldude260607@gmail.com>
2018-05-30 23:36:56 +0100

    Fixed syntax error and converted to two decimal places

--- Addons/Admin/auto-chat.lua
+++ Addons/Admin/auto-chat.lua
@@ -44,7 +44,7 @@ local commands = {
     ['aids']={'chat-bot.aids'},
     ['riot']={'chat-bot.riot'},
     ['lenny']={'chat-bot.lenny'},
-    ['evolution']=function(player) return {'chat_bot.current-evolution',#game.forces["enemy"].evolution_factor} end,
+    ['evolution']=function(player) return {'chat_bot.current-evolution',string.format('%.2f',game.forces['enemy'].evolution_factor)} end,
     --Jokes about food and drink
     ['whattoeat']={'chat-bot.food'},
     ['makepopcorn']=function(player) Server.new_thread{
