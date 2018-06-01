b9b43504a5b5368966f9513b039e02084a95b42c
JCA122204 <34488805+JCA122204@users.noreply.github.com>
2018-06-01 15:10:28 -0400

    Add /tp Command To Co-Exist With /bring And /go-to (#65)
    * Add !evolution to auto-chat.lua
    
    * Add !evolution to chat-bot.cfg
    
    * Fixed syntax error and converted to two decimal places
    
    * Add the /tp command to co-exist with /bring and /go-to
    
    * Add /tp command to co-exist with /bring and /go-to into admin.lua
    
    * Fix /tp
    
    * Fix admin.lua
    
    * Fixed Admin.tp call

--- Addons/Commands/tp.lua
+++ Addons/Commands/tp.lua
@@ -19,3 +19,13 @@ commands.add_command('bring', 'Bring a player to your location', {'player'}, fun
     if not player then player_return({'commands.invalid-player',args.player}) return commands.error end
     Admin.bring(player,event)
 end)
+
+commands.add_command('tp', 'Teleport a player to another player\'s location', {'from', 'to'}, function(event,args)
+    local from_player = Game.get_player(args.from)
+    if not from_player then player_return({'commands.invalid-player',args.from_player}) return commands.error end
+    
+    local to_player = Game.get_player(args.to)
+    if not to_player then player_return({'commands.invalid-player',args.to_player}) return commands.error end
+    
+    Admin.tp(from_player, to_player)
+end)
