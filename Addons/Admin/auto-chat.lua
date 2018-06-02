e96ecafc2e0cc34661ac6df7ea5ceca3e9d20fbf
sakamakj <38378426+sakamakj@users.noreply.github.com>
2018-06-03 01:10:16 +0200

    merge

--- Addons/Admin/auto-chat.lua
+++ Addons/Admin/auto-chat.lua
@@ -45,7 +45,7 @@ local commands = {
     ['riot']={'chat-bot.riot'},
     ['lenny']={'chat-bot.lenny'},
     ['wiki']={'chat-bot.wiki'},
-    ['evolution']=function(player) return {'chat_bot.current-evolution',string.format('%.2f',game.forces['enemy'].evolution_factor)} end,
+    ['evolution']=function(player) return {'chat-bot.current-evolution',string.format('%.2f',game.forces['enemy'].evolution_factor)} end,
     --Jokes about food and drink
     ['whattoeat']={'chat-bot.food'},
     ['makepopcorn']=function(player) Server.new_thread{
