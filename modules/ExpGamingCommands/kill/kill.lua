cf6e2838cac70b52392f057d26074ddf8a37edfb
Cooldude2606 <cooldude260607@gmail.com>
2018-06-24 19:33:54 +0100

    Added Kill Command

---
+++ modules/ExpGamingCommands/kill/kill.lua
@@ -0,0 +1,18 @@
+--- A full ranking system for factorio.
+-- @module ExpGamingCommands.kill
+-- @author Cooldude2606
+-- @license https://github.com/explosivegaming/scenario/blob/master/LICENSE
+
+local Game = require('FactorioStdLib.Game')
+
+--- Kills a player of a lower rank
+-- @command kill
+-- @param player the player to be killed
+commands.add_command('kill', 'Kills a player. No player name kills yourself.', {
+    ['player']={false,'player-rank-alive'}
+}, function(event,args)
+    local _player = Game.get_player(event)
+    local player = args.player
+    if player then player.character.die()
+    else _player.character.die() end
+end)
