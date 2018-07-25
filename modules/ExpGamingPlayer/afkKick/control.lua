5fac283d2b0da18994aee7efb1a71a49f40ef617
Cooldude2606 <cooldude260607@gmail.com>
2018-07-25 15:57:23 +0100

    Added ExpGamingPlayer.afkKick

---
+++ modules/ExpGamingPlayer/afkKick/control.lua
@@ -0,0 +1,38 @@
+--- A full ranking system for factorio.
+-- @module ExpGamingPlayer.playerInfo
+-- @author Cooldude2606
+-- @license https://github.com/explosivegaming/scenario/blob/master/LICENSE
+
+local Game = require('FactorioStdLib.Game')
+local Gui = require('ExpGamingCore.Gui')
+local Ranking -- hanndled on load
+local Sync -- hanndled on load
+
+function get_allowed_afk_time(player)
+    local rank
+    if Ranking then rank = Ranking.get_rank(player)
+    else if player.admin then return else rank = {base_afk_time=15} end end
+    local count = #game.connected_players
+    local base = rank.base_afk_time or false
+    if not base then return false end
+    return (base/5)*count
+end
+
+script.on_event(defines.events.on_tick,function(event)
+    if (game.tick%3600) ~= 0 then return end
+    for _,player in pairs(game.connected_players) do
+        local afk = #game.connected_players < 3 and 10 or get_allowed_afk_time(player)
+        if afk then
+            if player.afk_time > afk*3600 then game.kick_player(player,'AFK For Too Long ('..math.floor(afk)..' Minutes)') end
+        end
+    end
+end)
+
+return {
+    get_allowed_afk_time=get_allowed_afk_time,
+    on_init=function(self)
+        if loaded_modules['ExpGamingCore.Ranking'] then Ranking = require('ExpGamingCore.Ranking') end
+        if loaded_modules['ExpGamingCore.Sync'] then Sync = require('ExpGamingCore.Sync') end
+        if loaded_modules['ExpGamingCore.Server'] then require(module_path..'/src/server.lua',Sync) end
+    end
+}
---
+++ modules/ExpGamingPlayer/afkKick/src/server.lua
@@ -0,0 +1,25 @@
+local Sync = Sync
+Event[defines.events.on_tick] = nil
+script.on_init(function(event)
+    Server.new_thread{
+        name='afk-kick',
+    }:on_event('tick',function(self)
+        if (game.tick%3600) ~= 0 then return end
+        for _,player in pairs(game.connected_players) do
+            local afk = #game.connected_players < 3 and 10 or get_allowed_afk_time(player)
+            if afk then
+                if player.afk_time > afk*3600 then game.kick_player(player,'AFK For Too Long ('..math.floor(afk)..' Minutes)') end
+            end
+        end
+    end):on_event('error',function(self,err)
+        if Sync then
+            Sync.emit_embeded{
+                title='Auto Kick Error',
+                color=Color.to_hex(defines.text_color.bg),
+                description='Auto Kick Error - Closed Thread',
+                Error=err
+            }
+        end
+        self:close()
+    end):open()
+end)
