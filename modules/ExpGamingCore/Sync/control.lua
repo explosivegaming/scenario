af7e8b37ed70a4985cd29014a8d2c51bf97eb2b6
Cooldude2606 <cooldude260607@gmail.com>
2018-12-30 17:48:40 +0000

    Added Sync to /interface

--- modules/ExpGamingCore/Sync/control.lua
+++ modules/ExpGamingCore/Sync/control.lua
@@ -318,7 +318,7 @@ script.on_event('on_rocket_launched',Sync.emit_update)
 
 function Sync:on_init()
     if loaded_modules['ExpGamingCore.Gui'] then verbose('ExpGamingCore.Gui is installed; Loading gui src') require(module_path..'/src/gui',{Sync=Sync,module_path=module_path}) end
-    if loaded_modules['ExpGamingCore.Server'] then require('ExpGamingCore.Server') end
+    if loaded_modules['ExpGamingCore.Server'] then require('ExpGamingCore.Server').add_module_to_interface('Sync','ExpGamingCore.Sync') end
 end
 
 function Sync:on_post()
