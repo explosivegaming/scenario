cfbab34c518333550652b9dd58e5a062af94256d
Cooldude2606 <cooldude260607@gmail.com>
2019-03-24 20:06:43 +0000

    Added Adv Starting Items

--- config/file_loader.lua
+++ config/file_loader.lua
@@ -17,7 +17,8 @@ return {
     -- QoL Addons
     'modules.addons.chat-popups',
     'modules.addons.damage-popups',
-    'modules.addons.death-markers',
+    'modules.addons.death-logger',
+    'modules.addons.advanced-starting-items',
     -- Config Files
     'config.command_auth_admin', -- commands tagged with admin_only are blocked for non admins
     'config.command_auth_runtime_disable', -- allows commands to be enabled and disabled during runtime
