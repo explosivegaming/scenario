660397a6d76bc36c7eca80623204f8a2cc01d670
Cooldude2606 <cooldude260607@gmail.com>
2019-03-25 21:27:43 +0000

    Added compi :D

--- config/file_loader.lua
+++ config/file_loader.lua
@@ -20,6 +20,7 @@ return {
     'modules.addons.death-logger',
     'modules.addons.advanced-starting-items',
     'modules.addons.spawn-area',
+    'modules.addons.compilatron',
     -- Config Files
     'config.command_auth_admin', -- commands tagged with admin_only are blocked for non admins
     'config.command_auth_runtime_disable', -- allows commands to be enabled and disabled during runtime
