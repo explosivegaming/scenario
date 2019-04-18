837da3a4971dfd235e273c066947cc6cdce95454
Cooldude2606 <cooldude260607@gmail.com>
2019-04-18 15:37:00 +0100

    Added warning config

---
+++ config/warnings.lua
@@ -0,0 +1,19 @@
+--- Config file for the warning system, this is very similar to reports but is for the use of moderators rather than normal users.
+return {
+    actions = { -- what actions are taking at number of warnings
+        -- if a localized string is used then __1__ will by_player_name and __2__ will be the current warning count (auto inserted)
+        {'warnings.recived'},
+        {'warnings.recived'},
+        {'warnings.recived',{'warnings.pre-kick'}},
+        function(player,by_player_name,number_of_warnings)
+            game.kick_player(player,{'warnings.received',by_player_name,number_of_warnings,{'warnings.kick'}})
+        end,
+        {'warnings.recived',{'warnings.pre-pre-ban'}},
+        {'warnings.recived',{'warnings.pre-ban'}},
+        function(player,by_player_name,number_of_warnings)
+            game.ban_player(player,{'warnings.received',by_player_name,number_of_warnings,{'warnings.ban',{'info.website-link'}}})
+        end
+    },
+    temp_warning_cool_down=30, -- time for a temp warning (given by script) to be removed
+    temp_warning_limit=5 -- the number of temp warnings (given by script) that are allowed before full warnings are given
+}
