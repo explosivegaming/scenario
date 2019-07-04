b128483838e5a6382298deebbae7bbac738a5c2b
bbassie <badgamernl@gmail.com>
2019-07-04 10:21:03 +0200

    Cleanup bot

--- old/modules/DONE/WornPaths/src/placed.lua
+++
@@ -1,9 +0,0 @@
-return {
-    ['refined-concrete']=true,
-    ['refined-hazard-concrete-right']=true,
-    ['refined-hazard-concrete-left']=true,
-    ['concrete']=true,
-    ['hazard-concrete-right']=true,
-    ['hazard-concrete-left']=true,
-    ['stone-path']=true
-}
