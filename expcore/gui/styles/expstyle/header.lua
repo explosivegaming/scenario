2cd9c80804c7b3291f1aaa3bbb29af354d47ed3d
Cooldude2606 <cooldude260607@gmail.com>
2019-09-24 18:43:19 +0100

    Added more styles

--- expcore/gui/styles/expstyle/header.lua
+++ expcore/gui/styles/expstyle/header.lua
@@ -10,6 +10,7 @@ local right_align =
 Gui.new_concept('alignment')
 
 --[[-- A frame that acts as a header to a section of content
+@see frame
 @element header
 @tparam string tooltip the tooltip to show on the title
 @usage-- Concept Structure
@@ -17,6 +18,8 @@ Gui.new_concept('alignment')
 --> [header] - the header frame
 -->> header_caption - the lable with the title in it
 -->> header_content - the area to contain butons
+Gui.new_concept('header')
+:set_title('Example Header')
 ]]
 
 Gui.new_concept('frame')
