2b451c562ed90b6c20d70c14808be19a389d55e0
Cooldude2606 <cooldude260607@gmail.com>
2021-03-30 04:08:17 +0100

    Made requested changes

--- modules/commands/last-location.lua
+++ modules/commands/last-location.lua
@@ -11,8 +11,8 @@ require 'config.expcore.command_general_parse'
 -- @command last-location
 -- @tparam LuaPlayer player the player that you want a location of
 Commands.new_command('last-location', 'Sends you the last location of a player')
-:add_param('player', false, 'player-role')
-:set_flag('admin_only')
+:add_alias('location')
+:add_param('player', false, 'player')
 :register(function(player, action_player)
     local action_player_name_color = format_chat_player_name(action_player)
     player.print{'expcom-lastlocation.response', action_player_name_color, action_player.position.x, action_player.position.y}
