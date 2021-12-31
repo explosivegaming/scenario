ecc263faf85b34fc6056fb8d7bb2a7661a2f94e7
oof2win2 <honza.koco44@gmail.com>
2021-12-31 15:37:01 +0100

    ✨ config file for deconlog

---
+++ config/deconlog.lua
@@ -0,0 +1,8 @@
+-- @config Deconlog
+
+return {
+	decon_area = true, ---@setting decon_area whether to log when an area is being deconstructed
+	built_entity = true, ---@setting built_entity whether to log when an entity is built
+	mined_entity = true, ---@setting mined_entity whether to log when an entity is mined
+	fired_nuke = true, ---@setting fired_nuke whether to log when a nuke is fired
+}
