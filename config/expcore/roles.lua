d0d59d35d83a92cf67b078a01ec2ebb222779f2c
oof2win2 <honza.koco44@gmail.com>
2022-11-12 22:39:37 +0100

    fix(config): roles

--- config/expcore/roles.lua
+++ config/expcore/roles.lua
@@ -291,7 +291,7 @@ Roles.define_role_order{
 }
 
 Roles.override_player_roles{
-    ["oof2win2"]={"Senior Administrator","Moderator","Senior Backer","Supporter"},
+    ["Cooldude2606"]={"Senior Administrator","Moderator","Senior Backer","Supporter"},
     ["arty714"]={"Senior Administrator","Senior Backer","Supporter"},
     ["Drahc_pro"]={"Administrator","Moderator","Veteran","Member"},
     ["mark9064"]={"Administrator","Moderator","Member"},
