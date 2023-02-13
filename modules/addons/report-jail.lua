70412cadc177009b8bfd435eb1086c29b0c1a2c2
PHIDIAS <10016767+PHIDIAS0303@users.noreply.github.com>
2023-02-13 23:44:16 +0900

    Update report-jail.lua

--- modules/addons/report-jail.lua
+++ modules/addons/report-jail.lua
@@ -6,6 +6,8 @@ local config = require 'config.report-jail' --- @dep config.afk_kick
 local Jail = require 'modules.control.jail' ---@dep modules.control.jail
 local Reports = require 'modules.control.reports' --- @dep modules.control.reports
 local format_chat_player_name = _C.format_chat_player_name --- @dep expcore.common
+local has_role = Roles.player_has_role
+local get_roles = Roles.get_player_roles
 
 --- Returns the playtime of the reporter. Used when calculating the total playtime of all reporters
 local function reporter_playtime(_, by_player_name, _)
@@ -34,14 +36,16 @@ Event.add(Reports.events.on_player_reported, function(event)
     end
     
     if not moderator_count_bool then
-        if player.online_time ~= nil and player.MachinesRemoved ~= nil then
-            
-        end    
-    end
+        -- if player.online_time ~= nil and player.MachinesRemoved ~= nil then
+        if has_role(player, 'Member') or has_role(player, 'Veteran') then
+            -- Combined playtime is greater than 200% of the reported's playtime
+            if total_playtime < player.online_time * 2 then 
+                return 
+            end
 
-    if total_playtime < player.online_time*1.5 then return end
-    -- Combined playtime is greater than 150% of the reported's playtime
-    local player_name_color = format_chat_player_name(player)
-    Jail.jail_player(player, '<reports>', 'Reported by too many players, please wait for a moderator.')
-    game.print{'report-jail.jail', player_name_color}
+            local player_name_color = format_chat_player_name(player)
+            Jail.jail_player(player, '<reports>', 'Reported by too many players, please wait for a moderator.')
+            game.print{'report-jail.jail', player_name_color}
+        end
+    end
 end)
