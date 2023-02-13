2f945bad9b17297c69f079f8847af22d54a3e9d3
PHIDIAS <10016767+PHIDIAS0303@users.noreply.github.com>
2023-02-13 22:54:49 +0900

    Update deconlog.lua

--- modules/addons/deconlog.lua
+++ modules/addons/deconlog.lua
@@ -11,11 +11,17 @@ local filepath = "log/decon.log"
 local function add_log(data)
 	game.write_file(filepath, data .. "\n", true, 0) -- write data
 end
+
 local function get_secs ()
 	return format_time(game.tick, { hours = true, minutes = true, seconds = true, string = true })
 end
-local function pos_tostring (pos)
+
+local function pos_to_string (pos)
 	return tostring(pos.x) .. "," .. tostring(pos.y)
+end
+
+local function pos_to_gps_string (pos)
+	return "[gps=" .. tostring(pos.x) .. "," .. tostring(pos.y) .. "]"
 end
 
 Event.on_init(function()
@@ -26,7 +32,14 @@ if config.decon_area then
 	Event.add(defines.events.on_player_deconstructed_area, function (e)
 		local player = game.get_player(e.player_index)
 		if Roles.player_has_flag(player, "deconlog-bypass") then return end
-		add_log(get_secs() .. "," .. player.name .. ",decon_area," .. pos_tostring(e.area.left_top) .. "," .. pos_tostring(e.area.right_bottom))
+
+		for _, player_ in pairs(game.connected_players) do
+			if player_ ~= player then
+				game.players[player_].print{player.name .. " tried to deconstruct the area " .. pos_to_gps_string(e.area.left_top) .. " to " .. pos_to_gps_string(e.area.right_bottom) .. " but were not allowed."}
+			end
+		end
+
+		add_log(get_secs() .. "," .. player.name .. ",decon_area," .. pos_to_string(e.area.left_top) .. "," .. pos_to_string(e.area.right_bottom))
 	end)
 end
 
@@ -36,7 +49,7 @@ if config.built_entity then
 		local player = game.get_player(e.player_index)
 		if Roles.player_has_flag(player, "deconlog-bypass") then return end
 		local ent = e.created_entity
-		add_log(get_secs() .. "," .. player.name .. ",built_entity," .. ent.name .. "," .. pos_tostring(ent.position) .. "," .. tostring(ent.direction) .. "," .. tostring(ent.orientation))
+		add_log(get_secs() .. "," .. player.name .. ",built_entity," .. ent.name .. "," .. pos_to_string(ent.position) .. "," .. tostring(ent.direction) .. "," .. tostring(ent.orientation))
 	end)
 end
 
@@ -45,7 +58,7 @@ if config.mined_entity then
 		local player = game.get_player(e.player_index)
 		if Roles.player_has_flag(player, "deconlog-bypass") then return end
 		local ent = e.entity
-		add_log(get_secs() .. "," .. player.name .. ",mined_entity," .. ent.name .. "," .. pos_tostring(ent.position) .. "," .. tostring(ent.direction) .. "," .. tostring(ent.orientation))
+		add_log(get_secs() .. "," .. player.name .. ",mined_entity," .. ent.name .. "," .. pos_to_string(ent.position) .. "," .. tostring(ent.direction) .. "," .. tostring(ent.orientation))
 	end)
 end
 
@@ -57,7 +70,7 @@ if config.fired_rocket then
 		local item = ammo_inv[player.character.selected_gun_index]
 		if not item or not item.valid or not item.valid_for_read then return end
 		if item.name == "rocket" then
-			add_log(get_secs() .. "," .. player.name .. ",shot-rocket," .. pos_tostring(player.position) .. "," .. pos_tostring(player.shooting_state.position))
+			add_log(get_secs() .. "," .. player.name .. ",shot-rocket," .. pos_to_string(player.position) .. "," .. pos_to_string(player.shooting_state.position))
 		end
 	end)
 end
@@ -70,7 +83,7 @@ if config.fired_explosive_rocket then
 		local item = ammo_inv[player.character.selected_gun_index]
 		if not item or not item.valid or not item.valid_for_read then return end
 		if item.name == "explosive-rocket" then
-			add_log(get_secs() .. "," .. player.name .. ",shot-explosive-rocket," .. pos_tostring(player.position) .. "," .. pos_tostring(player.shooting_state.position))
+			add_log(get_secs() .. "," .. player.name .. ",shot-explosive-rocket," .. pos_to_string(player.position) .. "," .. pos_to_string(player.shooting_state.position))
 		end
 	end)
 end
@@ -83,7 +96,7 @@ if config.fired_nuke then
 		local item = ammo_inv[player.character.selected_gun_index]
 		if not item or not item.valid or not item.valid_for_read then return end
 		if item.name == "atomic-bomb" then
-			add_log(get_secs() .. "," .. player.name .. ",shot-nuke," .. pos_tostring(player.position) .. "," .. pos_tostring(player.shooting_state.position))
+			add_log(get_secs() .. "," .. player.name .. ",shot-nuke," .. pos_to_string(player.position) .. "," .. pos_to_string(player.shooting_state.position))
 		end
 	end)
 end
