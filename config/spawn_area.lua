020ee1d4ca6dba16ec8c58e6518220e2b9274e3c
PHIDIAS <10016767+PHIDIAS0303@users.noreply.github.com>
2023-02-13 15:15:16 +0900

    Update spawn_area.lua

--- config/spawn_area.lua
+++ config/spawn_area.lua
@@ -3,7 +3,7 @@
 
 return {
     spawn_area = { --- @setting spawn_area Settings relating to the whole spawn area
-        deconstruction_radius = 96, --- @setting deconstruction_radius All entities within this radius will be removed
+        deconstruction_radius = 64, --- @setting deconstruction_radius All entities within this radius will be removed
         tile_radius = 20,
         deconstruction_tile = 'refined-concrete', --- @setting deconstruction_tile Tile to be placed in the deconstruction radius, use nil for map gen
         landfill_radius = 96, --- @setting pattern_radius All water within this radius will be land filled
