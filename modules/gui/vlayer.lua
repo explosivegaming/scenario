33349ef7c81d83d0c7e11522328b66b6a1cf75dc
PHIDIAS <10016767+PHIDIAS0303@users.noreply.github.com>
2023-02-15 23:39:41 +0900

    Update vlayer.lua

--- modules/gui/vlayer.lua
+++ modules/gui/vlayer.lua
@@ -23,25 +23,30 @@ Net Power Production:
 local vlayer_container =
 Gui.element(function(event_trigger, parent)
     local player = Gui.get_player_from_element(parent)
-    local container = Gui.container(parent, event_trigger, 400)
+    local container = Gui.container(parent, event_trigger, 300)
 
     local header = Gui.header(container, 'VLAYER', '', true)
-    local solar_panel_display_title = Gui.centered_label(container, 150, 'Solar Panel')
-    local solar_panel_display_count = Gui.centered_label(container, 150, '0')
-    local accumulator_display_title = Gui.centered_label(container, 150, 'Accumulator')
-    local accumulator_display_count = Gui.centered_label(container, 150, '0')
+    local scroll_table = Gui.scroll_table(container, 300, 2)
+    local solar_panel_display_title = Gui.centered_label(scroll_table, 150, 'Solar Panel')
+    local solar_panel_display_count = Gui.centered_label(scroll_table, 150, '0')
+    local accumulator_display_title = Gui.centered_label(scroll_table, 150, 'Accumulator')
+    local accumulator_display_count = Gui.centered_label(scroll_table, 150, '0')
 
     -- Return the external container
     return container.parent
 end)
 :add_to_left_flow()
 
-Gui.left_toolbar_button("entity/solar-panel", {'vlayer.main-tooltip'}, vlayer_container, function (player)
-	return Roles.player_allowed(player, "gui/vlayer")
+Gui.left_toolbar_button('entity/solar-panel', {'vlayer.main-tooltip'}, vlayer_container, function (player)
+	return Roles.player_allowed(player, 'gui/vlayer')
 end)
 
 Event.on_nth_tick(60, function()
-    vlayer_container.solar_panel_display_count.caption = format_number(global.phi.vlayer.storage.item['solar-panel'])
-    vlayer_container.battery_display_count.caption = format_number(global.phi.vlayer.storage.item['accumulator'])
+    for _, player in pairs(game.connected_players) do
+        local frame = Gui.get_left_element(player, player_list_container)
+        local container = frame.container
+        
+        container.scroll.table.solar_panel_display_count.caption = format_number(global.phi.vlayer.storage.item['solar-panel'])
+        container.scroll.table.accumulator_display_count.caption = format_number(global.phi.vlayer.storage.item['accumulator'])
     end
 end)
