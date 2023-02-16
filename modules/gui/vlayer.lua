c7b829cdc172f78efbb262c439abc7860ce77974
PHIDIAS <10016767+PHIDIAS0303@users.noreply.github.com>
2023-02-16 16:59:53 +0900

    Update vlayer.lua

--- modules/gui/vlayer.lua
+++ modules/gui/vlayer.lua
@@ -6,6 +6,10 @@ local Colors = require 'utils.color_presets' --- @dep utils.color_presets
 local format_number = require('util').format_number
 
 --[[
+Item Count:
+Solar Panel: 344,000
+Accumulator: 369,000
+
 Power Production:
 Peak: 344 k x 60 kW
 Average sustained: 344 x 60 kW x 291 / 416
@@ -29,32 +33,63 @@ Gui.element(function(event_trigger, parent)
         name = 'solar_panel_display_title',
         type = 'label',
         caption = '[img=entity/solar-panel] Solar Panel',
+        style = 'heading_1_label'
     }
     local solar_panel_display_count =
     scroll_table.add{
         name = 'solar_panel_display_count',
         type = 'label',
-        caption = '0'
+        caption = '0',
+        style = 'heading_1_label'
     }
     local accumulator_display_title =
     scroll_table.add{
         name = 'accumulator_display_title',
         type = 'label',
         caption = '[img=entity/accumulator] Accumulator',
+        style = 'heading_1_label'
     }
     local accumulator_display_count =
     scroll_table.add{
         name = 'accumulator_display_count',
         type = 'label',
-        caption = '0'
+        caption = '0',
+        style = 'heading_1_label'
     }
+    
+    solar_panel_display_title.style.width=150
+    accumulator_display_title.style.width=150
 
-    --[[
-    local no_packs_style = no_packs_label.style
-    no_packs_style.padding = {2, 4}
-    no_packs_style.single_line = false
-    no_packs_style.width = 200
-    ]]
+    local power_production_peak_display_title =
+    scroll_table.add{
+        name = 'accumulator_display_title',
+        type = 'label',
+        caption = 'Power Production Peak',
+        style = 'heading_1_label'
+    }
+    local power_production_peak_display_count =
+    scroll_table.add{
+        name = 'accumulator_display_count',
+        type = 'label',
+        caption = '0 kW',
+        style = 'heading_1_label'
+    }
+    local power_production_sustained_display_title =
+    scroll_table.add{
+        name = 'accumulator_display_title',
+        type = 'label',
+        caption = 'Power Production Sustained',
+        style = 'heading_1_label'
+    }
+    local power_production_sustained_display_count =
+    scroll_table.add{
+        name = 'accumulator_display_count',
+        type = 'label',
+        caption = '0 kW',
+        style = 'heading_1_label'
+    }
+    power_production_peak_display_title.style.width=150
+    power_production_sustained_display_title.style.width=150
 
     -- Return the external container
     return container.parent
@@ -72,5 +107,8 @@ Event.on_nth_tick(60, function()
         
         container.scroll.table.solar_panel_display_count.caption = format_number(global.phi.vlayer.storage.item['solar-panel'])
         container.scroll.table.accumulator_display_count.caption = format_number(global.phi.vlayer.storage.item['accumulator'])
+        
+        container.scroll.table.power_production_peak_display_count.caption = format_number(global.phi.vlayer.storage.item['solar-panel'] * 60) .. ' kW'
+        container.scroll.table.power_production_sustained_display_count.caption = format_number(global.phi.vlayer.storage.item['solar-panel'] * 4365 / 104) .. ' kW'
     end
 end)
