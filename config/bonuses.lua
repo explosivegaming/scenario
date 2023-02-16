04ff7a6a713c67d21d54395cc6fb005987d53507
PHIDIAS <10016767+PHIDIAS0303@users.noreply.github.com>
2023-02-16 23:19:16 +0900

    Update bonuses.lua

--- config/bonuses.lua
+++ config/bonuses.lua
@@ -2,10 +2,52 @@
 -- @config Bonuses
 
 return {
-    character_mining_speed_modifier={0,3},
-    character_crafting_speed_modifier={0,3},
-    character_running_speed_modifier={0,3},
-    character_build_distance_bonus={0,20},
-    character_reach_distance_bonus={0,20},
-    character_inventory_slots_bonus={0,200}
+    {
+        name='character_mining_speed_modifier',
+        stage = {
+            '0' = 0.0,
+            '1' = 0.2,
+            '2' = 2.0
+        }
+    },
+    {
+        name='character_running_speed_modifier',
+        stage = {
+            '0' = 0.0,
+            '1' = 0.2,
+            '2' = 2.0
+        }
+    },
+    {
+        name='character_crafting_speed_modifier',
+        stage = {
+            '0' = 0.0,
+            '1' = 0.4,
+            '2' = 4.0
+        }
+    },
+    {
+        name='character_build_distance_bonus',
+        stage = {
+            '0' = 0,
+            '1' = 4,
+            '2' = 20
+        }
+    },
+    {
+        name='character_reach_distance_bonus',
+        stage = {
+            '0' = 0,
+            '1' = 4,
+            '2' = 20
+        }
+    },
+    {
+        name='character_inventory_slots_bonus',
+        stage = {
+            '0' = 0,
+            '1' = 20,
+            '2' = 100
+        }
+    }
 }
