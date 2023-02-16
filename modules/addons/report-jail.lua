73195160fc1ae5db5ba0a8f258bae5009921978d
PHIDIAS <10016767+PHIDIAS0303@users.noreply.github.com>
2023-02-16 17:59:06 +0900

    Update report-jail.lua

--- modules/addons/report-jail.lua
+++ modules/addons/report-jail.lua
@@ -18,9 +18,10 @@ end
 
 --- Tests the combined playtime of all reporters against the reported player
 --[[
-    If there is active moderator, 
+    If there is active moderator,
     then let moderator do it.
 ]]
+
 Event.add(Reports.events.on_player_reported, function(event)
     local player = game.get_player(event.player_index)
     local total_playtime = Reports.count_reports(player, reporter_playtime)
@@ -41,8 +42,8 @@ Event.add(Reports.events.on_player_reported, function(event)
         -- Combined playtime is greater than 200% of the reported's playtime
         -- 30 min = 108,000
 
-        if ((player.online_time * 2) > total_playtime) and (player.online_time > 108000) then 
-            return 
+        if ((player.online_time * 2) > total_playtime) and (player.online_time > 108000) then
+            return
         elseif #Reports.get_reports(player) > 1 then
             local player_name_color = format_chat_player_name(player)
             Jail.jail_player(player, '<reports>', 'Reported by too many players, please wait for a moderator.')
