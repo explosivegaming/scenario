4945c5a7ebcbfe36edcee5891e03c47e8431b8ed
PHIDIAS <10016767+PHIDIAS0303@users.noreply.github.com>
2023-02-18 00:26:21 +0900

    Update speed.lua

--- modules/commands/speed.lua
+++ modules/commands/speed.lua
@@ -10,10 +10,10 @@ Commands.new_command('speed', 'Set game speed')
 :add_param('amount', 'number-range', 0.1, 1)
 :set_flag('admin_only')
 :register(function(player, amount)
-    game.speed = amount
+    game.speed = string.format("%.3f", amount)
 
     for _, player_ in pairs(game.connected_players) do
-        player_.print{'expcom-speed.result', amount}
+        player_.print{'expcom-speed.result', string.format("%.3f", string.format("%.3f", amount))}
     end
 
     return Commands.success
