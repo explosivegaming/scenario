d5fe0c066ea454fd54ee40afb4fc89ccae7b1978
PHIDIAS <10016767+PHIDIAS0303@users.noreply.github.com>
2023-02-19 14:32:24 +0900

    Update teleport.lua

--- modules/commands/teleport.lua
+++ modules/commands/teleport.lua
@@ -6,6 +6,35 @@
 local Commands = require 'expcore.commands' --- @dep expcore.commands
 require 'config.expcore.command_general_parse'
 
+function teleport(from_player, to_player)
+    local surface = to_player.surface
+    local position = surface.find_non_colliding_position('character', to_player.position, 32, 1)
+    if not position then return false end -- return false if no new position
+    
+    if player.vehicle then
+        -- Teleport the entity
+        local entity = player.vehicle
+        local goto_position = surface.find_non_colliding_position(entity.name, position, 32, 1)
+        -- Surface teleport can only be done for players and cars at the moment. (with surface as an peramitor it gives this error)
+        if entity.type == "car" then
+            entity.teleport(goto_position, surface)
+        elseif surface.index == entity.surface.index then
+            -- Try teleport the entity
+            if not entity.teleport(goto_position) then
+                player.driving = false
+                -- Need to calculate new goto_position because entities have different collision boxes
+                goto_position = surface.find_non_colliding_position('character', position, 32, 1)
+                player.teleport(goto_position, surface)
+            end
+        end
+    else
+        -- Teleport the player
+        local goto_position = surface.find_non_colliding_position('character', position, 32, 1)
+        if player.driving then player.driving = false end
+        player.teleport(goto_position, surface)
+    end
+end
+
 local function teleport(from_player, to_player)
     local surface = to_player.surface
     local position = surface.find_non_colliding_position('character', to_player.position, 32, 1)
