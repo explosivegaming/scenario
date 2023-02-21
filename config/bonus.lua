bb65b3af89108c2f87f938921f3aa67b2f550a10
PHIDIAS <10016767+PHIDIAS0303@users.noreply.github.com>
2023-02-21 23:39:09 +0900

    Update bonus.lua

--- config/bonus.lua
+++ config/bonus.lua
@@ -4,48 +4,64 @@
 return {
     player = {
         {
-            name='character_mining_speed_modifier',
+            name = 'character_mining_speed_modifier',
+            enabled = true,
             max = 2
         },
         {
-            name='character_running_speed_modifier',
+            name = 'character_running_speed_modifier',
+            enabled = true,
             max = 2
         },
         {
-            name='character_crafting_speed_modifier',
+            name = 'character_crafting_speed_modifier',
+            enabled = true,
             max = 4
         },
         {
-            name='character_reach_distance_bonus',
+            name = 'character_reach_distance_bonus',
+            enabled = true,
             max = 20
         },
         {
-            name='character_resource_reach_distance_bonus',
+            name = 'character_resource_reach_distance_bonus',
+            enabled = true,
             max = 20
         },
         {
-            name='character_build_distance_bonus',
+            name = 'character_build_distance_bonus',
+            enabled = true,
             max = 20
         },
         {
-            name='character_item_pickup_distance_bonus',
+            name = 'character_item_pickup_distance_bonus',
+            enabled = true,
             max = 10
         },
         {
-            name='character_loot_pickup_distance_bonus',
+            name = 'character_loot_pickup_distance_bonus',
+            enabled = true,
             max = 10
         },
         {
-            name='character_item_drop_distance_bonus',
+            name = 'character_item_drop_distance_bonus',
+            enabled = true,
             max = 20
         },
         {
-            name='character_inventory_slots_bonus',
+            name = 'character_inventory_slots_bonus',
+            enabled = true,
             max = 100
         },
         {
-            name='character_health_bonus',
+            name = 'character_health_bonus',
+            enabled = true,
             max = 200
         }
+    },
+    force = {
+        name = 'laboratory_speed_modifier',
+        enabled = true,
+        max = 0
     }
 }
