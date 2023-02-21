765ef7d912052c2d5cbd6b2a49bf33f12d4f0432
PHIDIAS <10016767+PHIDIAS0303@users.noreply.github.com>
2023-02-21 17:26:31 +0900

    Update bonus.lua

--- config/bonus.lua
+++ config/bonus.lua
@@ -2,48 +2,50 @@
 -- @config Bonuses
 
 return {
-    {
-        name='character_mining_speed_modifier',
-        max = 2
-    },
-    {
-        name='character_running_speed_modifier',
-        max = 2
-    },
-    {
-        name='character_crafting_speed_modifier',
-        max = 4
-    },
-    {
-        name='character_reach_distance_bonus',
-        max = 20
-    },
-    {
-        name='character_resource_reach_distance_bonus',
-        max = 20
-    },
-    {
-        name='character_build_distance_bonus',
-        max = 20
-    },
-    {
-        name='character_item_pickup_distance_bonus',
-        max = 10
-    },
-    {
-        name='character_loot_pickup_distance_bonus',
-        max = 10
-    },
-    {
-        name='character_item_drop_distance_bonus',
-        max = 20
-    },
-    {
-        name='character_inventory_slots_bonus',
-        max = 100
-    },
-    {
-        name='character_health_bonus',
-        max = 200
+    player = {
+        {
+            name='character_mining_speed_modifier',
+            max = 2
+        },
+        {
+            name='character_running_speed_modifier',
+            max = 2
+        },
+        {
+            name='character_crafting_speed_modifier',
+            max = 4
+        },
+        {
+            name='character_reach_distance_bonus',
+            max = 20
+        },
+        {
+            name='character_resource_reach_distance_bonus',
+            max = 20
+        },
+        {
+            name='character_build_distance_bonus',
+            max = 20
+        },
+        {
+            name='character_item_pickup_distance_bonus',
+            max = 10
+        },
+        {
+            name='character_loot_pickup_distance_bonus',
+            max = 10
+        },
+        {
+            name='character_item_drop_distance_bonus',
+            max = 20
+        },
+        {
+            name='character_inventory_slots_bonus',
+            max = 100
+        },
+        {
+            name='character_health_bonus',
+            max = 200
+        }
     }
 }
