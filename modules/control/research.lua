cd3e3db0a68d8a412e8ef8bae074164d3d4ee31f
PHIDIAS <10016767+PHIDIAS0303@users.noreply.github.com>
2023-02-23 00:20:24 +0900

    Update research.lua

--- modules/control/research.lua
+++ modules/control/research.lua
@@ -2,7 +2,7 @@ local Event = require 'utils.event' --- @dep utils.event
 local config = require 'config.research' --- @dep config.research
 
 local function research_notification(event)
-    game.print{'[technology=' .. event.research.name .. '][color=255, 255, 255] Researched Complete - ' .. event.research.name .. '-' .. event.research.level .. '[/color]'}
+    game.print{'[color=255, 255, 255] Research Completed - [/color][technology=' .. event.research.name .. ']'}
 end
 
 if config.enabled then
