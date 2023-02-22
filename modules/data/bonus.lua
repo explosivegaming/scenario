ec2a1225940df92d9995f9f1d0b11a6013fd0186
PHIDIAS <10016767+PHIDIAS0303@users.noreply.github.com>
2023-02-22 23:43:56 +0900

    Update bonus.lua

--- modules/data/bonus.lua
+++ modules/data/bonus.lua
@@ -88,7 +88,7 @@ Event.add(defines.events.on_player_created, function(event)
 
     for k, v in pairs(config.force_bonus) do
         if config.force_bonus[k].enabled then
-            game.players[event.player_index].force[config.force_bonus[k].name] = config.force_bonus[k].max
+            game.players[event.player_index].force[config.force_bonus[k].name] = game.players[event.player_index].force[config.force_bonus[k].name] + config.force_bonus[k].max
         end
     end
 end)
