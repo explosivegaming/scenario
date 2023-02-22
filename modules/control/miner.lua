49a1ec08c6087dc20a38c3a4b5270e1f03d84e5e
PHIDIAS <10016767+PHIDIAS0303@users.noreply.github.com>
2023-02-22 15:04:21 +0900

    Update miner.lua

--- modules/control/miner.lua
+++ modules/control/miner.lua
@@ -1,11 +1,16 @@
 local Event = require 'utils.event_core' --- @dep utils.event_core
 
 local function auto_handle(ore)
-    if ore.name == 'uranium-ore' or ore.name == 'crude-oil' then
+    local miner = ore.surface.find_entities_filtered{area={{ore.position.x-1, ore.position.y-1}, {ore.position.x+1, ore.position.y+1}}, type='mining-drill'}
+    
+    if #miner = 0 then
+        return
+    end
+    
+    if miner.fluidbox and #miner.fluidbox > 0 then
         return nil
     end
 
-    local miner = ore.surface.find_entities_filtered{area={{ore.position.x-1, ore.position.y-1}, {ore.position.x+1, ore.position.y+1}}, type='mining-drill'}
     local resources = miner.surface.find_entities_filtered{area={{miner.position.x - miner.prototype.mining_drill_radius, miner.position.y - miner.prototype.mining_drill_radius}, {miner.position.x + miner.prototype.mining_drill_radius, miner.position.y + miner.prototype.mining_drill_radius}}, type='resource'}
 
     for _, resource in pairs(resources) do
