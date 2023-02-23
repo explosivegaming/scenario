c29b3187cde29dcae283943e18784eee462dc024
PHIDIAS <10016767+PHIDIAS0303@users.noreply.github.com>
2023-02-24 00:03:36 +0900

    Update vlayer.lua

--- modules/control/vlayer.lua
+++ modules/control/vlayer.lua
@@ -4,7 +4,7 @@
 local Gui = require 'expcore.gui' --- @dep expcore.gui
 local Roles = require 'expcore.roles' --- @dep expcore.roles
 local Event = require 'utils.event' --- @dep utils.event
-local config = require 'config.vlayer' --- @dep config.gui.player_list_actions
+local config = require 'config.vlayer' --- @dep config.vlayer
 local Colors = require 'utils.color_presets' --- @dep utils.color_presets
 local format_number = require('util').format_number
 
