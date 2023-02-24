c35457f7bdcc1042ed7239a21e0bc66120dc2ed7
PHIDIAS <10016767+PHIDIAS0303@users.noreply.github.com>
2023-02-24 18:56:02 +0900

    Update player_data.lua

--- modules/control/player_data.lua
+++ modules/control/player_data.lua
@@ -10,7 +10,7 @@ Commands.new_command('player-data-json', 'Player Data Json Lookup')
 :add_param('player_', false, 'player-alive')
 :add_alias('pdj')
 :register(function(player, player_)
-    local msg = {}
+    local msg = 'Player Data of ' .. player_ .. ' :'
 
     for _, name in pairs(PlayerData.Statistics.metadata.display_order) do
         local child = PlayerData.Statistics[name]
@@ -23,7 +23,11 @@ Commands.new_command('player-data-json', 'Player Data Json Lookup')
             value = format_number(value or 0)
         end
 
-        msg[{'exp-statistics.' .. name}] = value
+        msg = msg .. ' ' .. {'exp-statistics.' .. name} .. ' ' .. value
+
+        if _ % 8 == 0 then
+            msg = msg .. '\n'
+        end
     end
 
     game.player.print(msg)
