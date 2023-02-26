5e1e99ec7a996dae9d5d259bf0f111bc60559ccc
PHIDIAS <10016767+PHIDIAS0303@users.noreply.github.com>
2023-02-26 21:00:22 +0900

    Update bot_queue.lua

--- modules/commands/bot_queue.lua
+++ modules/commands/bot_queue.lua
@@ -8,7 +8,7 @@ require 'config.expcore.command_general_parse'
 
 Commands.new_command('bot-queue-get', 'Set bot queue')
 :set_flag('admin_only')
-:register(function(player)
+:register(function(_)
     local f = game.player.force.max_failed_attempts_per_tick_per_construction_queue
     local s = game.player.force.max_successful_attempts_per_tick_per_construction_queue
     game.print{'expcom-bot-queue.result', s, f}
@@ -18,9 +18,9 @@ end)
 Commands.new_command('bot-queue-set', 'Set bot queue')
 :add_param('amount', 'integer-range', 1, 3)
 :set_flag('admin_only')
-:register(function(player, amount)
+:register(function(_, amount)
     game.player.force.max_failed_attempts_per_tick_per_construction_queue = 3 * amount
     game.player.force.max_successful_attempts_per_tick_per_construction_queue = 1 * amount
-    player_.print{'expcom-bot-queue.result', 1 * amount, 3 * amount}
+    game.print{'expcom-bot-queue.result', 1 * amount, 3 * amount}
     return Commands.success
 end)
