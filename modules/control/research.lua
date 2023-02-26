a9bba02f3fc4aedc39f917069e2b81bb01758ee7
PHIDIAS <10016767+PHIDIAS0303@users.noreply.github.com>
2023-02-26 17:09:16 +0900

    Update research.lua

--- modules/control/research.lua
+++ modules/control/research.lua
@@ -1,6 +1,25 @@
 local Event = require 'utils.event' --- @dep utils.event
 local config = require 'config.research' --- @dep config.research
 
+local inf_res = {
+    -- Mining Productivity
+    'mining-productivity-4',
+    -- Robot Speed
+    'worker-robots-speed-6',
+    -- Laser Damage
+    'energy-weapons-damage-7',
+    -- Explosive Damage
+    'stronger-explosives-7',
+    -- Bullet Damage
+    'physical-projectile-damage-7',
+    -- Flame Damage
+    'refined-flammables-7',
+    -- Artillery Range
+    'artillery-shell-range-1',
+    -- Artillery Speed
+    'artillery-shell-speed-1'
+}
+
 local function research_notification(event)
     game.print{'research.msg', event.research.name}
 end
