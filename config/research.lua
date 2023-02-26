69678be55b32bee6e3e409ed9d22b3aa9ace9b8d
PHIDIAS <10016767+PHIDIAS0303@users.noreply.github.com>
2023-02-26 17:17:53 +0900

    Update research.lua

--- config/research.lua
+++ config/research.lua
@@ -2,5 +2,62 @@
 -- @config Research
 
 return {
-    enabled = true
+    enabled = true,
+    limit_research = false,
+    limit_research_list = {
+        {
+            -- Mining Productivity
+            name = 'mining-productivity-4',
+            enabled = false,
+            level = 4
+        },
+        {
+            -- Robot Speed
+            name = 'worker-robots-speed-6',
+            enabled = false,
+            level = 6
+        },
+        {
+            -- Laser Damage
+            name = 'energy-weapons-damage-7',
+            enabled = false,
+            level = 7
+        },
+        {
+            -- Explosive Damage
+            name = 'stronger-explosives-7',
+            enabled = false,
+            level = 7
+        },
+        {
+            -- Bullet Damage
+            name = 'physical-projectile-damage-7',
+            enabled = false,
+            level = 7
+        },
+        {
+            -- Flame Damage
+            name = 'refined-flammables-7',
+            enabled = false,
+            level = 7
+        },
+        {
+            -- Artillery Range
+            name = 'artillery-shell-range-1',
+            enabled = false,
+            level = 1
+        },
+        {
+            -- Artillery Speed
+            name = 'artillery-shell-speed-1',
+            enabled = false,
+            level = 1
+        }
+        {
+            -- Artillery Range
+            name = 'artillery-shell-range-1',
+            enabled = false,
+            level = 1
+        }
+    }
 }
