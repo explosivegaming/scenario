367ef2b76b0d15956433859a3d49b91c4725cf5d
PHIDIAS <10016767+PHIDIAS0303@users.noreply.github.com>
2023-02-26 23:08:14 +0900

    Update miner.lua

--- modules/control/miner.lua
+++ modules/control/miner.lua
@@ -1,30 +1,40 @@
 local Event = require 'utils.event_core' --- @dep utils.event_core
 local config = require 'config.miner' --- @dep config.miner
 
-local function auto_handle(ore)
-    local miner = ore.surface.find_entities_filtered{area={{ore.position.x-1, ore.position.y-1}, {ore.position.x+1, ore.position.y+1}}, type='mining-drill'}
+local function auto_handle(event)
+    local entities = event.entity.surface.find_entities_filtered{area={{event.entity.position.x-1, event.entity.position.y-1}, {event.entity.position.x+1, event.entity.position.y+1}}, type='mining-drill'}
     
-    if #miner == 0 then
+    if #entities == 0 then
         return
     end
 
-    if miner.fluidbox and #miner.fluidbox > 0 then
-        return nil
-    end
+    for _, entity in pairs(entities) do
+        if (entity.input_fluid_box and #entity.input_fluid_box > 0) or (entity.output_fluid_box and #entity.output_fluid_box > 0) then
+            return
+        end
 
-    local resources = miner.surface.find_entities_filtered{area={{miner.position.x - miner.prototype.mining_drill_radius, miner.position.y - miner.prototype.mining_drill_radius}, {miner.position.x + miner.prototype.mining_drill_radius, miner.position.y + miner.prototype.mining_drill_radius}}, type='resource'}
+        if ((math.abs(entity.position.x - event.entity.position.x) < entity.prototype.mining_drill_radius) and (math.abs(entity.position.y - event.entity.position.y) < entity.prototype.mining_drill_radius)) then
+            if entity.mining_target ~= nil and entity.mining_target.valid then
+                if entity.mining_target.amount > 0 then
+                    return
+                end
 
-    for _, resource in pairs(resources) do
-        if resource.amount > 0 then
-            return nil
+                local resources = entity.surface.find_entities_filtered{area={{entity.position.x - entity.prototype.mining_drill_radius, entity.position.y - entity.prototype.mining_drill_radius}, {entity.position.x + entity.prototype.mining_drill_radius, entity.position.y + entity.prototype.mining_drill_radius}}, type='resource'}
+
+                for _, resource in pairs(resources) do
+                    if resource.amount > 0 then
+                        return
+                    end
+                end
+
+                entity.order_deconstruction(entity.force)
+            end
         end
     end
-
-    miner.order_deconstruction(miner.force)
 end
 
 if config.enabled then
     Event.add(defines.events.on_resource_depleted, function(event)
-        auto_handle(event.entity)
+        auto_handle(event)
     end)
 end
