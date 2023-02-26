2af6d9ed4ceb089a61b2eae1efea9c701e5fe159
PHIDIAS <10016767+PHIDIAS0303@users.noreply.github.com>
2023-02-26 23:18:15 +0900

    Update cheat-mode.lua

--- modules/commands/cheat-mode.lua
+++ modules/commands/cheat-mode.lua
@@ -22,7 +22,7 @@ end)
 
 Commands.new_command('toggle-all-research', 'Toggles all research for your force.')
 :set_flag('admin_only')
-:register(function(_, player)
-    player.force.research_all_technologies()
+:register(function(_, __)
+    game.players[1].force.research_all_technologies()
     return Commands.success
 end)
