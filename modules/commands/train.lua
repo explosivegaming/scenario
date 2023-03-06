ee7fc08d80b22dbd049edf7c4a33eefbf6697312
PHIDIAS <10016767+PHIDIAS0303@users.noreply.github.com>
2023-03-06 17:49:03 +0900

    Create train.lua

---
+++ modules/commands/train.lua
@@ -0,0 +1,23 @@
+--[[-- Commands Module - Set Automatic Train
+    - Adds a command that set all train back to automatic
+    @commands Set Automatic Train
+]]
+
+local Commands = require 'expcore.commands' --- @dep expcore.commands
+require 'config.expcore.command_general_parse'
+local format_number = require('util').format_number
+
+Commands.new_command('manual-train', 'Set Automatic Train')
+:register(function(player)
+    local count = 0
+
+    for k,v in pairs(player.force.get_trains()) do
+        if v.manual_mode then
+            count = count + 1
+            v.manual_mode = false
+        end
+    end
+
+    game.print{'expcom-train.manual-result', format_number(count)}
+    return Commands.success
+end)
