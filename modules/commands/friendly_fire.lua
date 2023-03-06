9e0bb762b6f7f73739936ab6c41df1e783d41d8c
PHIDIAS <10016767+PHIDIAS0303@users.noreply.github.com>
2023-03-06 17:59:10 +0900

    Create friendly_fire.lua

---
+++ modules/commands/friendly_fire.lua
@@ -0,0 +1,16 @@
+/c  = false
+--[[-- Commands Module - Set Friendly Fire
+    - Adds a command that set all friendly fire
+    @commands Set Friendly Fire
+]]
+
+local Commands = require 'expcore.commands' --- @dep expcore.commands
+require 'config.expcore.command_general_parse'
+local format_number = require('util').format_number
+
+-- For Modded Server Use
+Commands.new_command('friendly-fire', 'Set Friendly Fire')
+:register(function(_)
+    game.forces.player.friendly_fire = not game.forces.player.friendly_fire
+    return Commands.success
+end)
