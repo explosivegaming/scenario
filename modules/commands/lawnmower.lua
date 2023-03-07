c529c1387c9b268980367b81e4cf8e329f9d014b
PHIDIAS <10016767+PHIDIAS0303@users.noreply.github.com>
2023-03-07 19:36:55 +0900

    Create lawnmower.lua

---
+++ modules/commands/lawnmower.lua
@@ -0,0 +1,21 @@
+--[[-- Commands Module - Lawnmower
+    - Adds a command that clean up biter corpse and nuclear hole
+    @commands Lawnmower
+]]
+
+local Commands = require 'expcore.commands' --- @dep expcore.commands
+require 'config.expcore.command_general_parse'
+
+Commands.new_command('lawnmower', 'Clean up biter corpse and nuclear hole')
+:add_param('range', false, 'integer-range', 1, 200)
+:register(function(player, range)
+	local entities = player.surface.find_entities_filtered{position=player.position, radius=range, type='corpse'}
+
+	for _, entity in pairs(entities) do
+		if not ((entity.name == 'transport-caution-corpse') or (entity.name == 'invisible-transport-caution-corpse')) then
+			entity.destroy()
+		end
+	end
+
+    return Commands.success
+end)
