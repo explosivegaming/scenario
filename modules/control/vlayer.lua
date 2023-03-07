8f287225552ffcdf29f3ce9fc75811f76ff806a5
PHIDIAS <10016767+PHIDIAS0303@users.noreply.github.com>
2023-03-07 14:08:30 +0900

    Update vlayer.lua

--- modules/control/vlayer.lua
+++ modules/control/vlayer.lua
@@ -43,24 +43,31 @@ local function vlayer_power_handle()
         if (v.power == nil) or (not v.power.valid)then
             global.vlayer.power.entity[k] = nil
         else
-            v.power.electric_buffer_size = vlayer_power_capacity
-            v.power.power_production = math.floor(vlayer_power_capacity / 60)
-            v.power.power_usage = math.floor(vlayer_power_capacity / 60)
-            local energy_average = math.floor((v.power.energy + global.vlayer.power.energy) / 2)
+            if config.use_vlayer then
+                v.power.electric_buffer_size = vlayer_power_capacity / 2
+                v.power.power_production = math.floor(vlayer_power_capacity / 60)
+                v.power.power_usage = math.floor(vlayer_power_capacity / 60)
 
-            if global.vlayer.power.energy < vlayer_power_capacity then
-                v.power.energy = energy_average
-                global.vlayer.power.energy = energy_average
-            elseif v.power.energy < vlayer_power_capacity then
-                local energy_change = vlayer_power_capacity - v.power.energy
+                local energy_average = math.floor((v.power.energy + global.vlayer.power.energy) / 2)
 
-                if energy_change < global.vlayer.power.energy then
-                    v.power.energy = v.power.energy + energy_change
-                    global.vlayer.power.energy = global.vlayer.power.energy - energy_change
-                else
-                    v.power.energy = v.power.energy + global.vlayer.power.energy
-                    global.vlayer.power.energy = 0
+                if global.vlayer.power.energy < vlayer_power_capacity then
+                    v.power.energy = energy_average
+                    global.vlayer.power.energy = energy_average
+                elseif v.power.energy < vlayer_power_capacity then
+                    local energy_change = vlayer_power_capacity - v.power.energy
+
+                    if energy_change < global.vlayer.power.energy then
+                        v.power.energy = v.power.energy + energy_change
+                        global.vlayer.power.energy = global.vlayer.power.energy - energy_change
+                    else
+                        v.power.energy = v.power.energy + global.vlayer.power.energy
+                        global.vlayer.power.energy = 0
+                    end
                 end
+            else
+                v.power.electric_buffer_size = vlayer_power_capacity
+                v.power.power_production = math.floor(vlayer_power_capacity / 60)
+                v.power.power_usage = math.floor(vlayer_power_capacity / 60)
             end
         end
     end
@@ -487,10 +494,18 @@ Event.on_nth_tick(config.update_tick, function()
         container.scroll.table.solar_panel_display_count.caption = format_number(global.vlayer.storage.item['solar-panel'])
         container.scroll.table.accumulator_display_count.caption = format_number(global.vlayer.storage.item['accumulator'])
         
-        container.scroll.table.power_production_peak_display_count.caption = format_number(math.floor(global.vlayer.storage.item['solar-panel'] * 0.06))
-        container.scroll.table.power_production_sustained_display_count.caption = format_number(math.floor(global.vlayer.storage.item['solar-panel'] * 873 / 20800))
-        
-        container.scroll.table.battery_max_display_count.caption = format_number((global.vlayer.storage.item['accumulator'] * 5) + ((config.energy_base_limit / 1000000) * #global.vlayer.power.entity))
-        container.scroll.table.battery_current_display_count.caption = format_number(math.floor(global.vlayer.power.energy / 1000000))
+        if config.use_vlayer then
+            container.scroll.table.power_production_peak_display_count.caption = format_number(math.floor(global.vlayer.storage.item['solar-panel'] * 0.06))
+            container.scroll.table.power_production_sustained_display_count.caption = format_number(math.floor(global.vlayer.storage.item['solar-panel'] * 873 / 20800))
+            
+            container.scroll.table.battery_max_display_count.caption = format_number((global.vlayer.storage.item['accumulator'] * 5) + ((config.energy_base_limit / 1000000) * #global.vlayer.power.entity))
+            container.scroll.table.battery_current_display_count.caption = format_number(math.floor(global.vlayer.power.energy / 1000000))
+        else
+            container.scroll.table.power_production_peak_display_count.caption = format_number(math.floor(global.vlayer.storage.item['solar-panel'] * 0.06))
+            container.scroll.table.power_production_sustained_display_count.caption = format_number(math.floor(global.vlayer.storage.item['solar-panel'] * 873 / 20800))
+            
+            container.scroll.table.battery_max_display_count.caption = format_number((global.vlayer.storage.item['accumulator'] * 5) + ((config.energy_base_limit / 1000000) * #global.vlayer.power.entity))
+            container.scroll.table.battery_current_display_count.caption = format_number(math.floor(global.vlayer.power.energy / 1000000))
+        end
     end
 end)
