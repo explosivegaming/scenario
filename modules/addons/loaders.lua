cb9301673c33fb48e90d57311e022edbca9ba294
PHIDIAS <10016767+PHIDIAS0303@users.noreply.github.com>
2023-03-07 21:31:49 +0900

    Update loaders.lua

--- modules/addons/loaders.lua
+++ modules/addons/loaders.lua
@@ -9,9 +9,15 @@ require 'config.expcore.command_general_parse'
 -- For Modded Server Use
 Commands.new_command('toggle-loader', 'Toggle Loaders')
 :register(function(_)
-    game.player.force.recipes.loader.enabled = not game.player.force.recipes.loader.enabled
-    game.player.force.recipes["fast-loader"].enabled = not game.player.force.recipes["fast-loader"].enabled
-    game.player.force.recipes["express-loader"].enabled = not game.player.force.recipes["express-loader"].enabled
+    local new_status = not game.player.force.recipes.loader.enabled
+    
+    game.player.force.recipes.loader.enabled = new_status
+    game.player.force.recipes["fast-loader"].enabled = new_status
+    game.player.force.recipes["express-loader"].enabled = new_status
 
+    game.player.force.recipes.loader.hidden = not new_status
+    game.player.force.recipes["fast-loader"].hidden = not new_status
+    game.player.force.recipes["express-loader"].hidden = not new_status
+    
     return Commands.success
 end)
