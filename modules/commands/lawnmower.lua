d6bb309262add9c1ddf4ea37d471118ca5cda25f
PHIDIAS <10016767+PHIDIAS0303@users.noreply.github.com>
2023-03-07 19:40:43 +0900

    Update lawnmower.lua

--- modules/commands/lawnmower.lua
+++ modules/commands/lawnmower.lua
@@ -9,8 +9,10 @@ require 'config.expcore.command_general_parse'
 Commands.new_command('lawnmower', 'Clean up biter corpse and nuclear hole')
 :add_param('range', false, 'integer-range', 1, 200)
 :register(function(player, range)
+    player.surface.destroy_decoratives({position=player.position, radius=range})
+    
 	local entities = player.surface.find_entities_filtered{position=player.position, radius=range, type='corpse'}
-
+    
 	for _, entity in pairs(entities) do
 		if not ((entity.name == 'transport-caution-corpse') or (entity.name == 'invisible-transport-caution-corpse')) then
 			entity.destroy()
