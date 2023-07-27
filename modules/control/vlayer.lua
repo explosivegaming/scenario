df015f3f774c425fba576c6314ea60f66ba0f66e
PHIDIAS <phidias@aperx.org>
2023-07-27 22:45:00 +0900

    .

--- modules/control/vlayer.lua
+++ modules/control/vlayer.lua
@@ -24,30 +24,25 @@ vlayer.circuit = {}
 vlayer.circuit.input = {}
 vlayer.circuit.output = {}
 
-vlayer.circuit.input[1].signal = {type='virtual', name='signal-P'}
-vlayer.circuit.input[2].signal = {type='virtual', name='signal-S'}
-vlayer.circuit.input[3].signal = {type='virtual', name='signal-M'}
-vlayer.circuit.input[4].signal = {type='virtual', name='signal-C'}
-vlayer.circuit.input[5].signal = {type='virtual', name='signal-D'}
-vlayer.circuit.input[6].signal = {type='virtual', name='signal-T'}
-vlayer.circuit.input[7].signal = {type='item', name='solar-panel'}
-vlayer.circuit.input[8].signal = {type='item', name='accumulator'}
-vlayer.circuit.input[9].signal = {type='item', name='landfill'}
+vlayer.circuit.input[1] = {signal={type='virtual', name='signal-P'}, count=1}
+vlayer.circuit.input[2] = {signal={type='virtual', name='signal-S'}, count=1}
+vlayer.circuit.input[3] = {signal={type='virtual', name='signal-M'}, count=1}
+vlayer.circuit.input[4] = {signal={type='virtual', name='signal-C'}, count=1}
+vlayer.circuit.input[5] = {signal={type='virtual', name='signal-D'}, count=1}
+vlayer.circuit.input[6] = {signal={type='virtual', name='signal-T'}, count=1}
+vlayer.circuit.input[7] = {signal={type='item', name='solar-panel'}, count=1}
+vlayer.circuit.input[8] = {signal={type='item', name='accumulator'}, count=1}
+vlayer.circuit.input[9] = {signal={type='item', name='landfill'}, count=1}
 
-vlayer.circuit.output[1].signal = {type='virtual', name='signal-P'}
-vlayer.circuit.output[2].signal = {type='virtual', name='signal-S'}
-vlayer.circuit.output[3].signal = {type='virtual', name='signal-M'}
-vlayer.circuit.output[4].signal = {type='virtual', name='signal-C'}
-vlayer.circuit.output[5].signal = {type='virtual', name='signal-D'}
-vlayer.circuit.output[6].signal = {type='virtual', name='signal-T'}
-vlayer.circuit.output[7].signal = {type='item', name='solar-panel'}
-vlayer.circuit.output[8].signal = {type='item', name='accumulator'}
-vlayer.circuit.output[9].signal = {type='item', name='landfill'}
-
-for i=1, #vlayer.circuit.input do
-    vlayer.circuit.input[i].count = 1
-    layer.circuit.output[i].count = 0
-end
+vlayer.circuit.output[1] = {signal={type='virtual', name='signal-P'}, count=0}
+vlayer.circuit.output[2] = {signal={type='virtual', name='signal-S'}, count=0}
+vlayer.circuit.output[3] = {signal={type='virtual', name='signal-M'}, count=0}
+vlayer.circuit.output[4] = {signal={type='virtual', name='signal-C'}, count=0}
+vlayer.circuit.output[5] = {signal={type='virtual', name='signal-D'}, count=0}
+vlayer.circuit.output[6] = {signal={type='virtual', name='signal-T'}, count=0}
+vlayer.circuit.output[7] = {signal={type='item', name='solar-panel'}, count=0}
+vlayer.circuit.output[8] = {signal={type='item', name='accumulator'}, count=0}
+vlayer.circuit.output[9] = {signal={type='item', name='landfill'}, count=0}
 
 vlayer.storage.item['solar-panel'] = 0
 vlayer.storage.item['accumulator'] = 0
