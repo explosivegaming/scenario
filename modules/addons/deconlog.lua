53d0acc6970999118c7e3381b7f5e9916ac52747
PHIDIAS <phidias@aperx.org>
2023-08-06 02:31:53 +0900

    .

--- modules/addons/deconlog.lua
+++ modules/addons/deconlog.lua
@@ -43,7 +43,11 @@ if config.decon_area then
 		if Roles.player_has_flag(player, "deconlog-bypass") then
 			return
 		end
-		print_to_players(true, player.name .. ' tried to deconstruct the area ' .. pos_to_gps_string(e.area.left_top) .. ' to ' .. pos_to_gps_string(e.area.right_bottom) .. ' but were not allowed.')
+
+		if ((e.area.left_top.x + e.area.right_bottom.x) * (e.area.left_top.y + e.area.right_bottom.y)) > 10000 then
+			print_to_players(true, player.name .. ' tried to deconstruct the area ' .. pos_to_gps_string(e.area.left_top) .. ' to ' .. pos_to_gps_string(e.area.right_bottom) .. ' but were not allowed.')
+		end
+
 		add_log(get_secs() .. "," .. player.name .. ",decon_area," .. pos_to_string(e.area.left_top) .. "," .. pos_to_string(e.area.right_bottom))
 	end)
 end
