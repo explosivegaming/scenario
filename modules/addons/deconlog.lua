d93b32a8a9dbb7b90889b2b8066db13617aa0311
PHIDIAS <phidias@aperx.org>
2023-08-09 03:53:52 +0900

    update

--- modules/addons/deconlog.lua
+++ modules/addons/deconlog.lua
@@ -44,7 +44,7 @@ if config.decon_area then
 			return
 		end
 
-		local items = e.surface.find_entities_filtered{area=e.area, force=e.player.force}
+		local items = e.surface.find_entities_filtered{area=e.area, force=player.force}
 		if items > 250 then
 			print_to_players(true, player.name .. ' tried to deconstruct the area ' .. pos_to_gps_string(e.area.left_top) .. ' to ' .. pos_to_gps_string(e.area.right_bottom) .. ' that have ' .. items .. ' items, but were not allowed.')
 		end
