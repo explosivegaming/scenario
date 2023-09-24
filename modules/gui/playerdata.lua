d767a50ec277adc054a92103981c7c31c2869e3e
PHIDIAS <phidias@aperx.org>
2023-09-24 18:57:27 +0900

    .

--- modules/gui/playerdata.lua
+++ modules/gui/playerdata.lua
@@ -16,7 +16,7 @@ local pd_disp_n = {
 pd_container =
 Gui.element(function(event_trigger, parent)
     local container = Gui.container(parent, event_trigger, 400)
-    local scroll_table_1 = Gui.scroll_table(container, 400, 4, 'pd_st_1')
+    local scroll_table_1 = Gui.scroll_table(container, 400, 2, 'pd_st_1')
     local player_list = {}
 
     for _, player in pairs(game.connected_players) do
@@ -27,7 +27,14 @@ Gui.element(function(event_trigger, parent)
         type = 'drop-down',
         name = 'pd_display_1p',
         items = player_list,
-        selected_index = 1
+        selected_index = 0
+    }
+
+    scroll_table_1.add{
+        type = 'button',
+        name = 'pd_display_1b',
+        caption = 'Update',
+        style = 'button'
     }
 
     local scroll_table_2 = Gui.scroll_table(container, 400, 4, 'pd_st_2')
@@ -45,14 +52,25 @@ Gui.element(function(event_trigger, parent)
 
         label(scroll_table_2)
 
-        label =
-        Gui.element{
-            name = 'pd_display_' .. name .. '_d',
-            type = 'label',
-            caption = ''
-        }:style{
-            maximal_width = 120
-        }
+        if name == 'Playtime' or name == 'AfkTime' then
+            label =
+            Gui.element{
+                name = 'pd_display_' .. name .. '_d',
+                type = 'label',
+                caption = '0 m'
+            }:style{
+                maximal_width = 120
+            }
+        else
+            label =
+            Gui.element{
+                name = 'pd_display_' .. name .. '_d',
+                type = 'label',
+                caption = '0'
+            }:style{
+                maximal_width = 120
+            }
+        end
 
         label(scroll_table_2)
     end
@@ -61,11 +79,11 @@ Gui.element(function(event_trigger, parent)
 end)
 :add_to_left_flow()
 
-Gui.left_toolbar_button('armor/power-armor-mk2', 'Player Data GUI', pd_container, function(player)
+Gui.left_toolbar_button('item/power-armor-mk2', 'Player Data GUI', pd_container, function(player)
 	return Roles.player_allowed(player, 'gui/playerdata')
 end)
 
-local function gui_update()
+local function gui_player_list_update()
     local player_list = {}
 
     for _, player in pairs(game.connected_players) do
@@ -78,30 +96,34 @@ local function gui_update()
     end
 end
 
+local function gui_player_data_update(player)
+    local frame = Gui.get_left_element(player, pd_container)
+
+    for _, name in pairs(PlayerData.Statistics.metadata.display_order) do
+        if name == 'Playtime' or name == 'AfkTime' then
+            frame.container['pd_st_2'].table['pd_display_' .. name .. '_d'].caption = format_time((PlayerData.Statistics[name]:get(player.name) or 0) * 3600, {hours=true, minutes=true, seconds=true, time=true, string=true})
+        else
+            frame.container['pd_st_2'].table['pd_display_' .. name .. '_d'].caption = format_number(PlayerData.Statistics[name]:get(player.name) or 0)
+        end
+    end
+end
+
 Event.add(defines.events.on_player_joined_game, function(_)
-    gui_update()
+    gui_player_list_update()
 end)
 
 Event.add(defines.events.on_player_left_game, function(_)
-    gui_update()
+    gui_player_list_update()
 end)
-
---[[
-Event.on_nth_tick(60, function()
-
-end)
-]]
 
 Event.add(defines.events.on_gui_elem_changed, function(event)
-    if event.element.name == 'pd_display_1p' then
-        local frame = Gui.get_left_element(game.players[event.player_index], pd_container)
+    if event.element.name == 'pd_display_1b' then
+        gui_player_data_update(game.players[event.player_index])
+    end
+end)
 
-        for _, name in pairs(PlayerData.Statistics.metadata.display_order) do
-            if name == 'Playtime' or name == 'AfkTime' then
-                frame.container['pd_st_2'].table['pd_display_' .. name .. '_d'].caption = format_time((PlayerData.Statistics[name]:get(game.players[event.player_index].name) or 0) * 3600, {hours=true, minutes=true, seconds=true, time=true, string=true})
-            else
-                frame.container['pd_st_2'].table['pd_display_' .. name .. '_d'].caption = format_number(PlayerData.Statistics[name]:get(game.players[event.player_index].name) or 0)
-            end
-        end
+Event.add(defines.events.on_gui_click, function(event)
+    if event.element.name == 'pd_display_1p' then
+        gui_player_data_update(game.players[event.player_index])
     end
 end)
