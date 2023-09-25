510fe8f74573c10493f4782cd98ca0c2f241d867
PHIDIAS <phidias@aperx.org>
2023-09-25 21:53:47 +0900

    Revert "."
    This reverts commit 166e39705c610a926a2e4e0bb2ae59dd5446e88e.

--- config/module.lua
+++ config/module.lua
@@ -5,6 +5,12 @@ return {
     machine_prod_disallow = {
         ['beacon'] = true
     },
+    machine_type = {
+        ['assembling-machine'] = true,
+        ['electric-furnace'] = true,
+        ['lab'] = true,
+        ['mining-drill'] = true
+    },
     machine = {
         ['electric-mining-drill'] = 'effectivity-module',
         ['assembling-machine-2'] = 'productivity-module',
