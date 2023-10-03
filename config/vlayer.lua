061c84303bbe1e41b0ef71f974c9b33c75db7558
PHIDIAS <phidias@aperx.org>
2023-10-03 18:30:30 +0900

    redo

--- config/vlayer.lua
+++ config/vlayer.lua
@@ -2,12 +2,9 @@
 -- @config Vlayer
 
 return {
-    enabled = true,
     update_tick = 10,
-    -- 10 MJ
-    energy_base_limit = 10000000,
     land = {
-        enabled = true,
+        enabled = false,
         tile = 'landfill',
         result = 4,
         requirement = {
@@ -17,151 +14,87 @@ return {
     },
     always_day = false,
     battery_limit = true,
-    -- setting to a value greater than 1 will allow for wireless energy transfer
+    -- setting to a value greater than 1
+    -- will allow wireless energy or item transfer
     interface_limit = {
-        storage_input = 1,
-        energy = 1,
-        circuit = 1
+        ['storage_input'] = 1,
+        ['energy'] = 1,
+        ['circuit'] = 1
     },
-    print_out = {
-        ['electric-energy-interface'] = 'energy interface',
-        ['constant-combinator'] = 'circuit output',
-        ['logistic-chest-storage'] = 'storage input'
-    },
-    gui = {
-        style = 'heading_1_label',
-        type = 'label',
-        content = {
-            {
-                title = 'Storage',
-                type = nil,
-                name = nil
-            },
-            {
-                title = '',
-                type = nil,
-                name = nil
-            },
-            {
-                title = '[img=entity/solar-panel] Solar Panel',
-                type = nil,
-                name = nil
-            },
-            {
-                title = 0,
-                type = 'item',
-                name = 'solar-panel'
-            },
-            {
-                title = '[img=entity/accumulator] Accumulator',
-                type = nil,
-                name = nil
-            },
-            {
-                title = 0,
-                type = 'item',
-                name = 'accumulator'
-            },
-            {
-                title = '[virtual-signal=signal-L] Landfill',
-                type = nil,
-                name = nil
-            },
-            {
-                title = 0,
-                type = 'signal',
-                name = 7
-            },
-            {
-                title = '[virtual-signal=signal-A] Solar Available',
-                type = nil,
-                name = nil
-            },
-            {
-                title = 0,
-                type = 'signal',
-                name = 8
-            },
-            {
-                title = '[virtual-signal=signal-B] Acc Available',
-                type = nil,
-                name = nil
-            },
-            {
-                title = 0,
-                type = 'signal',
-                name = 9
-            },
-            {
-                title = 'Power Production',
-                type = nil,
-                name = nil
-            },
-            {
-                title = 'MW',
-                type = nil,
-                name = nil
-            },
-            {
-                title = '[virtual-signal=signal-P] Peak',
-                type = nil,
-                name = nil
-            },
-            {
-                title = '0',
-                type = 'signal',
-                name = 1
-            },
-            {
-                title = '[virtual-signal=signal-S] Sustained',
-                type = nil,
-                name = nil
-            },
-            {
-                title = '0',
-                type = 'signal',
-                name = 2
-            },
-            {
-                title = 'Battery',
-                type = nil,
-                name = nil
-            },
-            {
-                title = 'MJ',
-                type = nil,
-                name = nil
-            },
-            {
-                title = '[virtual-signal=signal-M] Max',
-                type = nil,
-                name = nil
-            },
-            {
-                title = '0',
-                type = 'signal',
-                name = 3
-            },
-            {
-                title = '[virtual-signal=signal-C] Current',
-                type = nil,
-                name = nil
-            },
-            {
-                title = '0',
-                type = 'signal',
-                name = 4
-            },
-            {
-                title = 'Convert',
-                type = nil,
-                name = nil
-            },
-            {
-                title = '',
-                type = nil,
-                name = nil
-            }
+    init_item = {
+        ['solar-panel'] = {
+            enabled = true,
+            circuit = true,
+            value = 10
+        },
+        ['accumulator'] = {
+            enabled = true,
+            circuit = true,
+            value = 10
+        },
+        ['landfill'] = {
+            enabled = false,
+            circuit = true,
+            value = 100
         }
+    },
+    -- for modded power
+    init_item_m = {
+        ['solar-panel-2'] = {
+            n = 'solar-panel',
+            m = 4
+        },
+        ['solar-panel-3'] = {
+            n = 'solar-panel',
+            m = 16
+        },
+        ['solar-panel-4'] = {
+            n = 'solar-panel',
+            m = 64
+        },
+        ['solar-panel-5'] = {
+            n = 'solar-panel',
+            m = 256
+        },
+        ['solar-panel-6'] = {
+            n = 'solar-panel',
+            m = 1024
+        },
+        ['solar-panel-7'] = {
+            n = 'solar-panel',
+            m = 4096
+        },
+        ['solar-panel-8'] = {
+            n = 'solar-panel',
+            m = 16384
+        },
+        ['accumulator-2'] = {
+            n = 'accumulator',
+            m = 4
+        },
+        ['accumulator-3'] = {
+            n = 'accumulator',
+            m = 16
+        },
+        ['accumulator-4'] = {
+            n = 'accumulator',
+            m = 64
+        },
+        ['accumulator-5'] = {
+            n = 'accumulator',
+            m = 256
+        },
+        ['accumulator-6'] = {
+            n = 'accumulator',
+            m = 1024
+        },
+        ['accumulator-7'] = {
+            n = 'accumulator',
+            m = 4096
+        },
+        ['accumulator-8'] = {
+            n = 'accumulator',
+            m = 16384
+        },
     }
 }
