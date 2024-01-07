d4bfa8a22c033fa4eed1893d2b413e0cee8aa9db
Cooldude2606 <25043174+Cooldude2606@users.noreply.github.com>
2024-01-07 21:38:29 +0000

    Fix loading of malformed data

--- modules/gui/toolbar.lua
+++ modules/gui/toolbar.lua
@@ -407,11 +407,13 @@ ToolbarState:on_load(function(player_name, value)
     local element_hash = {}
     for index, id in ipairs(value[1]) do
         local element = datastore_id_map[id]
-        element_hash[element.uid] = true
-        elements[index] = {
-            element_uid = element.uid,
-            favourite = favourites[id] or false,
-        }
+        if element and not element_hash[element.uid] then
+            element_hash[element.uid] = true
+            elements[index] = {
+                element_uid = element.uid,
+                favourite = favourites[id] or false,
+            }
+        end
     end
 
     -- Add any in the default state that are missing
