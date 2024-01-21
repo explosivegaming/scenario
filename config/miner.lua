60f2430c9a1ead8626a1042e9fbe4e993062f887
PHIDIAS <phidias@aperx.org>
2024-01-21 13:39:54 +0900

    Update miner.lua

--- config/miner.lua
+++ config/miner.lua
@@ -2,8 +2,6 @@
 -- @config miner
 
 return {
-    -- check for fluid
-    fluid = true,
-    -- check for chest
-    chest = true
+    fluid = true, --- @setting fluid When true, checks for for fluid pipes when removing miners
+    chest = true --- @setting chest When true, checks for for chest when removing miners
 }
