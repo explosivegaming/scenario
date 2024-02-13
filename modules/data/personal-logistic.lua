87ffbe7ca498abb5fb7e362cf460db38353a8761
PHIDIAS <phidias@aperx.org>
2024-02-14 00:52:19 +0900

    Update personal-logistic.lua

--- modules/data/personal-logistic.lua
+++ modules/data/personal-logistic.lua
@@ -21,49 +21,50 @@ local function pl(type, target, amount)
         c(config.start + v.key)
     end
 
-    if (amount == -1) then
+    if (amount < 0) then
         return
+    end
 
-    else
-        local stats = target.force.item_production_statistics
+    local stats = target.force.item_production_statistics
 
-        for k, v in pairs(config.request) do
-            local v_min = math.ceil(v.min * amount)
-            local v_max = math.ceil(v.max * amount)
+    for k, v in pairs(config.request) do
+        local v_min = math.ceil(v.min * amount)
+        local v_max = math.ceil(v.max * amount)
 
-            if v.stack ~= nil and v.stack ~= 1 and v.type ~= 'weapon' then
-                v_min = math.floor(v_min / v.stack) * v.stack
-                v_max = math.ceil(v_max / v.stack) * v.stack
-            end
+        if v.stack ~= nil and v.stack ~= 1 and v.type ~= 'weapon' then
+            v_min = math.floor(v_min / v.stack) * v.stack
+            v_max = math.ceil(v_max / v.stack) * v.stack
+        end
 
-            if v.upgrade_of == nil then
-                if v.type ~= nil then
-                    if stats.get_input_count(k) < config.production_required[v.type] then
-                        if v_min > 0 then
-                            if v_min == v_max then
-                                v_min = math.floor((v_max * 0.5) / v.stack) * v.stack
-                            end
-                        else
-                            v_min = 0
+        if v.upgrade_of == nil then
+            if v.type ~= nil then
+                if stats.get_input_count(k) < config.production_required[v.type] then
+                    if v_min > 0 then
+                        if v_min == v_max then
+                            v_min = math.floor((v_max * 0.5) / v.stack) * v.stack
                         end
+
+                    else
+                        v_min = 0
                     end
                 end
+            end
 
-                s(config.start + v.key, {name=k, min=v_min, max=v_max})
+            s(config.start + v.key, {name=k, min=v_min, max=v_max})
 
-            else
-                if v.type ~= nil then
-                    if stats.get_input_count(k) >= config.production_required[v.type] then
-                        s(config.start + v.key, {name=k, min=v_min, max=v_max})
-                        local vuo = v.upgrade_of
+        else
+            if v.type ~= nil then
+                if stats.get_input_count(k) >= config.production_required[v.type] then
+                    s(config.start + v.key, {name=k, min=v_min, max=v_max})
+                    local vuo = v.upgrade_of
 
-                        while (vuo ~= nil) do
-                            s(config.start + config.request[vuo].key, {name=vuo, min=0, max=0})
-                            vuo = config.request[vuo].upgrade_of
-                        end
-                    else
-                        s(config.start + v.key, {name=k, min=0, max=v_max})
+                    while (vuo ~= nil) do
+                        s(config.start + config.request[vuo].key, {name=vuo, min=0, max=0})
+                        vuo = config.request[vuo].upgrade_of
                     end
+
+                else
+                    s(config.start + v.key, {name=k, min=0, max=v_max})
                 end
             end
         end
@@ -80,11 +81,13 @@ Commands.new_command('personal-logistic', 'Set Personal Logistic (-1 to cancel a
                 pl('s', player.selected, amount / 10)
                 return Commands.success
             end
+
         else
             pl('p', player, amount / 10)
             return Commands.success
         end
+
     else
-        player.print('Player logistic not researched')
+        player.print('Personal Logistic not researched')
     end
 end)
