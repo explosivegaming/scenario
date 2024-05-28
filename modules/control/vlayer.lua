1ee1e8b4d03040a733adccac918f6ca8ed0c9ec7
PHIDIAS <phidias@aperx.org>
2024-05-28 17:43:05 +0900

    Update vlayer.lua

--- modules/control/vlayer.lua
+++ modules/control/vlayer.lua
@@ -336,10 +336,8 @@ function vlayer.create_output_interface(surface, position, circuit, last_user)
 
     if circuit then
         for k, _ in pairs(circuit) do
-            if next(circuit[k]) ~= nil then
-                for _, v in pairs(circuit[k]) do
-                    interface.connect_neighbour({wire=defines.wire_type[k], target_entity=v})
-                end
+            for _, v in pairs(circuit[k]) do
+                interface.connect_neighbour({wire=defines.wire_type[k], target_entity=v})
             end
         end
     end
@@ -460,10 +458,8 @@ function vlayer.create_circuit_interface(surface, position, circuit, last_user)
 
     if circuit then
         for k, _ in pairs(circuit) do
-            if next(circuit[k]) ~= nil then
-                for _, v in pairs(circuit[k]) do
-                    interface.connect_neighbour({wire=defines.wire_type[k], target_entity=v})
-                end
+            for _, v in pairs(circuit[k]) do
+                interface.connect_neighbour({wire=defines.wire_type[k], target_entity=v})
             end
         end
     end
