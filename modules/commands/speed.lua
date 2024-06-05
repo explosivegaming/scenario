24dc7b4b7a390a5085b5345a01b48f3415adafd2
PHIDIAS <phidias@aperx.org>
2024-06-06 01:13:30 +0900

    Update speed.lua

--- modules/commands/speed.lua
+++ modules/commands/speed.lua
@@ -9,7 +9,8 @@ require 'config.expcore.command_general_parse'
 Commands.new_command('game-speed', 'Set game speed')
 :add_param('amount', 'number-range', 0.2, 1)
 :set_flag('admin_only')
-:register(function(_, amount)
+:register(function(player, amount)
     game.speed = math.round(amount, 3)
-    return Commands.success{'expcom-speed.result', string.format('%.3f', amount)}
+    game.print{'expcom-speed.result', player.name, string.format('%.3f', amount)}
+    return Commands.success
 end)
