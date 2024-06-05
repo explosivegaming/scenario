2c0308fc43bbb3b0e07ddce118726f50cb30567f
PHIDIAS <phidias@aperx.org>
2024-06-06 01:16:11 +0900

    Update bot-queue.lua

--- modules/commands/bot-queue.lua
+++ modules/commands/bot-queue.lua
@@ -20,5 +20,6 @@ Commands.new_command('bot-queue-set', 'Set bot queue')
 :register(function(player, amount)
     player.force.max_successful_attempts_per_tick_per_construction_queue = 3 * amount
     player.force.max_failed_attempts_per_tick_per_construction_queue = 1 * amount
-    return Commands.success{'expcom-bot-queue.result', 3 * amount, 1 * amount}
+    game.print{'expcom-bot-queue.result', player.name, 3 * amount, 1 * amount}
+    return Commands.success
 end)
