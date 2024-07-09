368cff75d550484e6759b13cf03842ef565314eb
PHIDIAS <phidias@aperx.org>
2024-07-10 01:45:15 +0900

    Update module.lua

--- config/module.lua
+++ config/module.lua
@@ -4,48 +4,48 @@ return {
     module_slot_max = 4,
     machine = {
         ['electric-mining-drill'] = {
-            module = 'effectivity-module',
-            prod = true
+            ['module'] = 'effectivity-module',
+            ['prod'] = true
         },
         ['pumpjack'] = {
-            module = 'effectivity-module',
-            prod = true
+            ['module'] = 'effectivity-module',
+            ['prod'] = true
         },
         ['assembling-machine-2'] = {
-            module = 'productivity-module',
-            prod = true
+            ['module'] = 'productivity-module',
+            ['prod'] = true
         },
         ['assembling-machine-3'] = {
-            module = 'productivity-module-3',
-            prod = true
+            ['module'] = 'productivity-module-3',
+            ['prod'] = true
         },
         ['electric-furnace'] = {
-            module = 'productivity-module-3',
-            prod = true
+            ['module'] = 'productivity-module-3',
+            ['prod'] = true
         },
         ['beacon'] = {
-            module = 'speed-module-3',
-            prod = false
+            ['module'] = 'speed-module-3',
+            ['prod'] = false
         },
         ['oil-refinery'] = {
-            module = 'productivity-module-3',
-            prod = true
+            ['module'] = 'productivity-module-3',
+            ['prod'] = true
         },
         ['chemical-plant'] = {
-            module = 'productivity-module-3',
-            prod = true
+            ['module'] = 'productivity-module-3',
+            ['prod'] = true
         },
         ['centrifuge'] = {
-            module = 'productivity-module-3',
-            prod = true
+            ['module'] = 'productivity-module-3',
+            ['prod'] = true
         },
         ['lab'] = {
-            module = 'productivity-module-3',
-            prod = true
+            ['module'] = 'productivity-module-3',
+            ['prod'] = true
         },
         ['rocket-silo'] = {
-            module = 'productivity-module-3',
-            prod = true
+            ['module'] = 'productivity-module-3',
+            ['prod'] = true
         }
     },
     module_allowed = {
