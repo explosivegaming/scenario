180c2c7bf3d12014c22efb2d7ff9b66e99d488f8
PHIDIAS <phidias@aperx.org>
2024-08-05 04:48:06 +0900

    Create lawnmower.lua

---
+++ modules/addons/lawnmower.lua
@@ -0,0 +1,56 @@
+--[[-- Addon Lawnmower
+    - Adds a command that clean up biter corpse and nuclear hole
+    @addon Lawnmower
+]]
+
+local Commands = require 'expcore.commands' --- @dep expcore.commands
+local Event = require 'utils.event' --- @dep utils.event
+require 'config.expcore.command_general_parse'
+
+Commands.new_command('lawnmower', 'Clean up biter corpse, decoratives and nuclear hole')
+:add_param('range', false, 'integer-range', 1, 200)
+:register(function(player, range)
+	local tile_to_do = {}
+
+    player.surface.destroy_decoratives({position=player.position, radius=range})
+
+	local entities = player.surface.find_entities_filtered{position=player.position, radius=range, type='corpse'}
+
+	for _, entity in pairs(entities) do
+		if (entity.name ~= 'transport-caution-corpse' and entity.name ~= 'invisible-transport-caution-corpse') then
+			entity.destroy()
+		end
+	end
+
+	local tiles = player.surface.find_tiles_filtered{position=player.position, radius=range, name={'nuclear-ground'}}
+
+	for _, tile in pairs(tiles) do
+		table.insert(tile_to_do, {name='grass-1', position=tile.position})
+	end
+
+	player.surface.set_tiles(tile_to_do)
+
+    return Commands.success
+end)
+
+local function destroy_decoratives(entity, surface, area)
+    if entity.type ~= 'entity-ghost' and entity.type ~= 'tile-ghost' and entity.prototype.selectable_in_game then
+        game.surfaces[surface].destroy_decoratives{area=area}
+    end
+end
+
+Event.add(defines.events.on_built_entity, function(event)
+    destroy_decoratives(event.created_entity, event.created_entity.surface_index, event.created_entity.selection_box)
+end)
+
+Event.add(defines.events.on_robot_built_entity, function(event)
+    destroy_decoratives(event.created_entity, event.created_entity.surface_index, event.created_entity.selection_box)
+end)
+
+Event.add(defines.events.script_raised_built, function(event)
+    destroy_decoratives(event.entity, event.entity.surface_index, event.entity.selection_box)
+end)
+
+Event.add(defines.events.script_raised_revive, function(event)
+    destroy_decoratives(event.entity, event.entity.surface_index, event.entity.selection_box)
+end)
