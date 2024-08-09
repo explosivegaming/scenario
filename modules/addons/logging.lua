17ed6d52cad0dbba791a124490b47de24204eedc
PHIDIAS <phidias@aperx.org>
2024-08-10 04:29:23 +0900

    Update logging.lua

--- modules/addons/logging.lua
+++ modules/addons/logging.lua
@@ -10,47 +10,58 @@ local function add_log(data)
 end
 
 Event.add(defines.events.on_rocket_launched, function(event)
-    if config.rocket_launch_display[event.rocket.force.rockets_launched] then
-        add_log('[ROCKET] ' .. event.rocket.force.rockets_launched .. ' rockets launched')
+    if event and event.rocket and event.rocket.force and event.rocket.force.rockets_launched then
+        if config.rocket_launch_display[event.rocket.force.rockets_launched] then
+            add_log('[ROCKET] ' .. event.rocket.force.rockets_launched .. ' rockets launched')
+        end
     end
 end)
 
 Event.add(defines.events.on_pre_player_died, function(event)
-    if event.cause then
-        if event.cause.type == 'character' then
-            add_log('[DEATH] ' .. game.players[event.player_index].name .. ' died because of ' .. (game.players[event.cause.player.index].name or ' unknown reason'))
+    if event and event.player_index then
+        if event.cause then
+            if event.cause.type and event.cause.type == 'character' and event.cause.player and event.cause.player.index then
+                add_log('[DEATH] ' .. game.players[event.player_index].name .. ' died because of ' .. (game.players[event.cause.player.index].name or ' unknown reason'))
+
+            else
+                add_log('[DEATH] ' .. game.players[event.player_index].name .. ' died because of ' .. (event.cause.name or ' unknown reason'))
+            end
 
         else
-            add_log('[DEATH] ' .. game.players[event.player_index].name .. ' died because of ' .. (event.cause.name or ' unknown reason'))
+            add_log('[DEATH] ' .. game.players[event.player_index].name .. ' died because of unknown reason')
         end
-
-    else
-        add_log('[DEATH] ' .. game.players[event.player_index].name .. ' died because of unknown reason')
-	end
+    end
 end)
 
 Event.add(defines.events.on_research_finished, function(event)
-    if event.by_script then
-        return
-    end
+    if event and event.research then
+        if event.by_script then
+            return
+        end
 
-    if not event.research.level then
-        add_log('[RES] ' .. string.match(event.research.name, '^(.-)%-%d+$'):gsub('-', ' ') .. ' has been researched')
-        return
-    end
+        if event.research.level and event.research.level > 1 then
+            add_log('[RES] ' .. string.match(event.research.name, '^(.-)%-%d+$'):gsub('-', ' ') .. ' at level ' .. (event.research.level - 1) .. ' has been researched')
 
-    if (event.research.level - 1) == 0 then
-        add_log('[RES] ' .. string.match(event.research.name, '^(.-)%-%d+$'):gsub('-', ' ') .. ' has been researched')
-
-    else
-        add_log('[RES] ' .. string.match(event.research.name, '^(.-)%-%d+$'):gsub('-', ' ') .. ' at level ' .. (event.research.level - 1) .. ' has been researched')
+        else
+            add_log('[RES] ' .. string.match(event.research.name, '^(.-)%-%d+$'):gsub('-', ' ') .. ' has been researched')
+        end
     end
 end)
 
 Event.add(defines.events.on_player_joined_game, function(event)
-    add_log('[JOIN] ' .. game.players[event.player_index].name .. ' joined the game')
+    if event and event.player_index then
+        add_log('[JOIN] ' .. game.players[event.player_index].name .. ' joined the game')
+    end
 end)
 
 Event.add(defines.events.on_player_left_game, function(event)
-    add_log('[LEAVE] ' .. game.players[event.player_index].name .. (config.disconnect_reason[event.reason] or config.disconnect_reason[defines.disconnect_reason.quit]))
+    if event and event.player_index then
+        if event.reason then
+            add_log('[LEAVE] ' .. game.players[event.player_index].name .. config.disconnect_reason[event.reason])
+
+        else
+            add_log('[LEAVE] ' .. game.players[event.player_index].name .. config.disconnect_reason[defines.disconnect_reason.quit])
+
+        end
+    end
 end)
