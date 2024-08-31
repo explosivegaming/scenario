758d13f292a1d96884326d9c9a01392a754e3c16
PHIDIAS <phidias@aperx.org>
2024-09-01 01:56:00 +0900

    Update spectate.lua

--- modules/commands/spectate.lua
+++ modules/commands/spectate.lua
@@ -9,11 +9,16 @@ require 'config.expcore.command_general_parse'
 
 --- Toggles spectator mode for the caller
 -- @command spectate
-Commands.new_command('spectate', 'Toggles spectator mode')
+Commands.new_command('spectate', {'expcom-spectate.descr-spectate'})
 :register(function(player)
     if Spectate.is_spectating(player) then
         Spectate.stop_spectate(player)
+
     else
+        if Spectate.is_following(player) then
+            Spectate.stop_follow(player)
+        end
+
         Spectate.start_spectate(player)
     end
 end)
@@ -21,13 +26,18 @@ end)
 --- Enters follow mode for the caller, following the given player.
 -- @command follow
 -- @tparam LuaPlayer player The player that will be followed
-Commands.new_command('follow', 'Start following a player in spectator')
+Commands.new_command('follow', {'expcom-spectate.descr-follow'})
 :add_alias('f')
 :add_param('player', false, 'player-online')
 :register(function(player, action_player)
     if player == action_player then
        return Commands.error{'expcom-spectate.follow-self'}
+
     else
+        if Spectate.is_spectating(player) then
+            Spectate.stop_spectate(player)
+        end
+
         Spectate.start_follow(player, action_player)
     end
 end)
