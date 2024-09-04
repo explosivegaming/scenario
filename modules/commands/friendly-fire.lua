bf45a0c9e6607315b1f4be5fb934ffaaac94a02f
PHIDIAS <phidias@aperx.org>
2024-09-05 01:52:00 +0900

    Update friendly-fire.lua

--- modules/commands/friendly-fire.lua
+++ modules/commands/friendly-fire.lua
@@ -7,7 +7,7 @@ local Commands = require 'expcore.commands' --- @dep expcore.commands
 require 'config.expcore.command_general_parse'
 
 -- For Modded Server Use
-Commands.new_command('toggle-friendly-fire', {'expcom-ff.descr'})
+Commands.new_command('toggle-friendly-fire', {'expcom-ff.description'})
 :add_param('force', true, 'force')
 :set_defaults{force=function(player)
     return player.force
