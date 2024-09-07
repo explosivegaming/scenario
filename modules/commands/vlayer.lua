17ca742b08d4028bdb0afbb0e626842a34ac084a
PHIDIAS <phidias@aperx.org>
2024-09-08 05:38:12 +0900

    Update vlayer.lua

--- modules/commands/vlayer.lua
+++ modules/commands/vlayer.lua
@@ -5,11 +5,11 @@ local Commands = require 'expcore.commands' --- @dep expcore.commands
 require 'config.expcore.command_general_parse'
 local vlayer = require 'modules.control.vlayer'
 
-Commands.new_command('vlayer-info', {'vlayer.description-vi'})
+Commands.new_command('vlayer-info', 'Vlayer Info')
 :register(function(_)
     local c = vlayer.get_circuits()
 
-    for ck, cv in pairs(c) do
-        Commands.print(cv .. ' : ' .. ck)
+    for k, v in pairs(c) do
+        Commands.print(v .. ' : ' .. k)
     end
 end)
