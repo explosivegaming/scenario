22bdfea3d12760a00344ddfb20de3e218ca5127b
PHIDIAS <phidias@aperx.org>
2024-09-09 15:40:44 +0900

    Update bonus.lua

--- config/bonus.lua
+++ config/bonus.lua
@@ -21,10 +21,10 @@ return {
         base = 200
     },
     gui_display_width = {
-        half = 160,
+        half = 150,
         label = 70,
         slider = 180,
-        count = 70
+        count = 50
     },
     conversion = {
         ['cmms'] = 'character_mining_speed_modifier',
