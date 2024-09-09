ca20d1643cc1ec319c8cae6c41e703f09af9b7b4
PHIDIAS <phidias@aperx.org>
2024-09-09 15:44:21 +0900

    Update roles.lua

--- modules/commands/roles.lua
+++ modules/commands/roles.lua
@@ -19,8 +19,10 @@ Commands.new_command('assign-role', {'expcom-roles.description-assign-role'}, 'A
 :add_alias('rpromote', 'assign', 'role', 'add-role')
 :register(function(player, action_player, role)
     local player_highest = Roles.get_player_highest_role(player)
-    if player_highest and (player_highest.index < role.index) then
+
+    if player_highest.index < role.index then
         Roles.assign_player(action_player, role, player.name)
+
     else
         return Commands.error{'expcom-roles.higher-role'}
     end
@@ -37,8 +39,10 @@ Commands.new_command('unassign-role', {'expcom-roles.description-unassign-role'}
 :add_alias('rdemote', 'unassign', 'rerole', 'remove-role')
 :register(function(player, action_player, role)
     local player_highest = Roles.get_player_highest_role(player)
-    if player_highest and (player_highest.index < role.index) then
+
+    if player_highest.index < role.index then
         Roles.unassign_player(action_player, role, player.name)
+
     else
         return Commands.error{'expcom-roles.higher-role'}
     end
@@ -53,22 +57,27 @@ Commands.new_command('list-roles', {'expcom-roles.description-list-roles'}, 'Lis
 :register(function(_, player)
     local roles = Roles.config.order
     local message = {'expcom-roles.list'}
+
     if player then
         roles = Roles.get_player_roles(player)
     end
+
     for index, role in pairs(roles) do
         role = Roles.get_role_from_any(role)
         local colour = role.custom_color or Colours.white
         local role_name = format_chat_colour_localized(role.name, colour)
         if index == 1 then
             message = {'expcom-roles.list', role_name}
+
             if player then
                 local player_name_colour = format_chat_player_name(player)
                 message = {'expcom-roles.list-player', player_name_colour, role_name}
             end
+
         else
             message = {'expcom-roles.list-element', message, role_name}
         end
     end
+
     return Commands.success(message)
 end)
